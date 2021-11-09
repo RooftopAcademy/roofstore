94f607ef8feed99ca2c5b5c5c8c4706cfd51234d
Scarlet <smichelle05a@gmail.com>
2021-11-09 15:13:33 -0300

    add icon component and cdn

--- src/Components/Icon.js
+++ src/Components/Icon.js
@@ -1,6 +1,25 @@
 
-function Icon() {
-    return <div>...</div>
+function Icon(props) {
+  const icons = {
+    cart: "fas fa-shopping-cart",
+    buscar: "fas fa-search",
+    pinMarker: "fas fa-map-marker-alt",
+    historial: "far fa-clock",
+    home: "fas fa-home",
+    ventas: "fas fa-tag",
+    like: "far fa-heart",
+    liked: "fas fa-heart",
+    fullStar: "fas fa-star",
+    halfStar: "fas fa-star-half-alt",
+    star: "far fa-star",
+    arrow: "fas fa-angle-right",
+    enter: "fas fa-arrow-right",
+    down: "fas fa-angle-down",
+    lightning: "fas fa-bolt",
+  }
+    return <>
+    <i className={`${icons[props.icon]}`}></i>
+    </>
 }
 
 export default Icon
