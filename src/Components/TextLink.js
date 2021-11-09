fa8c1647fa74ed4c4971097f963bec81f904ac9b
Mauri548 <mauricioferreyra548@gmail.com>
2021-11-09 15:40:26 -0300

    use react-dom-router Link and customize class

--- src/Components/TextLink.js
+++ src/Components/TextLink.js
@@ -1,6 +1,7 @@
+import {Link} from 'react-router-dom'
 
-function TextLink({children, url}) {
-    return <a className="link link-color" href={url}>{children}</a>
+function TextLink({children, url, className="link-color" }) {
+    return <Link className={`link ${className}`} to={url}>{children}</Link>
 }
 
 export default TextLink
