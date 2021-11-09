b00ee3ef5e407c2f3fca39570f477d9c35b9cf05
Kevin Dev <kevin.developer71@gmail.com>
2021-11-09 11:34:32 -0600

    se Crea componente BannerImage

--- src/Components/BannerImage.js
+++ src/Components/BannerImage.js
@@ -1,6 +1,14 @@
-
 function BannerImage() {
-    return <div>...</div>
+  return (
+    <div>
+      <img
+      width="100%"
+      height="255px"
+        src="https://http2.mlstatic.com/D_NQ_908991-MLA48135975612_112021-OO.webp"
+        alt="slider image 1"
+      />
+    </div>
+  )
 }
 
 export default BannerImage
