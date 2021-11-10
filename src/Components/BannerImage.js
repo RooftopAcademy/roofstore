c87e80822cd515b636a1b7aaa7a82f2338d5fe46
Kevin Dev <kevin.developer71@gmail.com>
2021-11-10 11:33:08 -0600

    fix: se recibe src y alt por props

--- src/Components/BannerImage.js
+++ src/Components/BannerImage.js
@@ -1,11 +1,17 @@
-function BannerImage() {
+/* eslint-disable jsx-a11y/img-redundant-alt */
+function BannerImage(props) {
+  const {
+    src = "https://http2.mlstatic.com/D_NQ_908991-MLA48135975612_112021-OO.webp",
+    alt = "Banner Image",
+  } = props
+  
   return (
     <div>
       <img
-      width="100%"
-      height="255px"
-        src="https://http2.mlstatic.com/D_NQ_908991-MLA48135975612_112021-OO.webp"
-        alt="slider image 1"
+        width="100%"
+        height="255px"
+        src={src}
+        alt={alt}
       />
     </div>
   )
