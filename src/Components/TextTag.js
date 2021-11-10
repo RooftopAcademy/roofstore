3ead7e41aa7ead57fb924a63c890e6decacde68e
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-10 10:50:57 -0300

    Modified TexTag, now is a component useful

--- src/Components/TextTag.js
+++ src/Components/TextTag.js
@@ -1,9 +1,37 @@
 /**
  * Example: "OFERTA DEL DIA"
  */
+import TextLink from './TextLink'
 
-function TextTag() {
-    return <div>...</div>
+function TextTag({
+    text = '',
+    url = null,
+    txtColor = 'txt-white',
+    bgColor = 'bg-blue',
+}) {
+
+    let content;
+
+    if (url) {
+        content = (<TextLink className={txtColor} url={url}>{text}</TextLink>)
+    } else {
+        content = (<p>{text}</p>)
+    }
+
+    return (
+        <div
+            className={txtColor + ' txt-bold ' + bgColor}
+            style={{
+                display: 'inline-flex',
+                padding: '3px 4px',
+                borderRadius: '3px',
+                fontSize: '10px',
+                lineHeight: '1em',
+            }}
+        >
+            {content}
+        </div>
+    )
 }
 
 export default TextTag
