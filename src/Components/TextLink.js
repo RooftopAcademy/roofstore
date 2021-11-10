d5f93e47650c21579242a621ed0939faa2e3e20b
Mauri548 <mauricioferreyra548@gmail.com>
2021-11-10 14:51:31 -0300

    added required url property

--- src/Components/TextLink.js
+++ src/Components/TextLink.js
@@ -1,11 +1,18 @@
 import {Link} from 'react-router-dom'
 
-function TextLink({children, url = "#", className="link-color" }) {
-    return (
-        <Link className={`link ${className}`} to={url}>
-            {children}
-        </Link>
-    )
+function TextLink({children, url, className="link-color" }) {
+
+    if (url) {
+        return (
+            <Link className={`link ${className}`} to={url}>
+                {children}
+            </Link>
+        )
+    }
+    
+    console.error('El componente <TextLink /> deber recibir una url como propiedad')
+    
+    return (<div>{children}</div>)
 }
 
 export default TextLink
