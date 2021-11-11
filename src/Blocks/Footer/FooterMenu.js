ac56bd9f21673cc7ab49741e02ad0155126fcfc5
Fernando Alba <fernando.alba@live.com.ar>
2021-11-11 12:25:04 -0300

    refactor FooterMenu with style classes

--- src/Blocks/Footer/FooterMenu.js
+++ src/Blocks/Footer/FooterMenu.js
@@ -3,19 +3,19 @@ import TextLink from "../../Components/TextLink"
 function FooterMenu() {
     return (
         <div className='row'>
-            <div className='col col-6'>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Mi cuenta</TextLink>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Historial</TextLink>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Favoritos</TextLink>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Categorias</TextLink>
-                <TextLink url={'/login'} className={'txt-black d-block'}>Ayuda</TextLink>
+            <div className='col col-6 d-flex fd-col'>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Mi cuenta</TextLink>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Historial</TextLink>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Favoritos</TextLink>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Categorias</TextLink>
+                <TextLink url={'/login'} className={'txt-black'}>Ayuda</TextLink>
             </div>
-            <div className='col col-6'>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Mis Compras</TextLink>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Ofertas</TextLink>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Tiendas Oficiales</TextLink>
-                <TextLink url={'/login'} className={'txt-black m-bottom-0 d-block'}>Mercado Puntos</TextLink>
-                <TextLink url={'/login'} className={'txt-black d-block'}>Vender</TextLink>
+            <div className='col col-6 d-flex fd-col'>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Mis Compras</TextLink>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Ofertas</TextLink>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Tiendas Oficiales</TextLink>
+                <TextLink url={'/login'} className={'txt-black m-bottom-0'}>Mercado Puntos</TextLink>
+                <TextLink url={'/login'} className={'txt-black'}>Vender</TextLink>
             </div>
         </div>
     )
