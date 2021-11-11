45c04736689771cbd62566dae660061b472a71d9
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-11 11:48:42 -0300

    Modified name file: OnSalePage => OffersPage

---
+++ src/Pages/Public/OffersPage.js
@@ -0,0 +1,10 @@
+import WebsiteLayout from '../../Layouts/WebsiteLayout'
+
+function OffersPage() {
+    return (
+        <WebsiteLayout>
+        </WebsiteLayout>
+    )
+}
+
+export default OffersPage
