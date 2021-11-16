10e6110445f5ecc28a7c2503751440972c533a9d
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-16 11:29:19 -0300

    Refactor to component TextPaginator and Paginator

--- src/Pages/Public/OffersPage/Blocks/Pagination.js
+++ src/Pages/Public/OffersPage/Blocks/Pagination.js
@@ -1,22 +1,24 @@
 import PageNumberPaginator from "../Components/PageNumberPaginator"
 import TextPaginator from "../Components/TextPaginator"
 
-export default function Pagination() {
+export default function Pagination({ numPages }) {
 
   const urlSearchParams = new URLSearchParams(window.location.search)
-  const currentPage = Object.fromEntries(urlSearchParams.entries()).page || 1
+  const currentPage = Number(Object.fromEntries(urlSearchParams.entries()).page) || 1
+  const numNextPage = currentPage + 1
+  const numPreviousPage = currentPage - 1
+
   const previousHidden = (currentPage > 1) ? '' : 'OffersPage-v-hidden'
-  const numNextPage = Number(currentPage) + 1
-  const numPreviousPage = Number(currentPage) - 1
+  const nextHidden = (currentPage === numPages) ? 'OffersPage-v-hidden' : ''
 
   const previousText = "Anterior"
   const nextText = "Siguiente"
 
   return (
     <div className="row">
-      <TextPaginator className={previousHidden} text={previousText} numPage={numPreviousPage} />
+      <TextPaginator className={previousHidden} previousText={previousText} numPage={numPreviousPage} />
       <PageNumberPaginator currentPage={currentPage}/>
-      <TextPaginator text={nextText} numPage={numNextPage} />
+      <TextPaginator className={nextHidden} nextText={nextText} numPage={numNextPage} />
     </div>
   )
 }
--- src/Pages/Public/OffersPage/Components/TextPaginator.js
+++ src/Pages/Public/OffersPage/Components/TextPaginator.js
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom'
 import Icon from '../../../../Components/Icon'
 
-export default function TextPaginator({ numPage, className, text }) {
-  const nextText = "Siguiente"
-  const previousText = "Anterior"
+export default function TextPaginator({ numPage, className, previousText, nextText }) {
+
+  const content = (previousText)
+    ? <>
+      <i className="fas fa-angle-left"></i>
+      <p className="OffersPage-m-left OffersPage-m-right">{previousText} </p>
+    </>
+
+    : <>
+      <p className="OffersPage-m-right">{nextText}</p>
+      <Icon icon="arrow-right" />
+    </>
 
   return (
     <Link
         to={`/offers?page=${numPage}`}
         className={"col link OffersPage-txt-light-grey OffersPage-ai-center " + className}
     >
-      {text === nextText && <p className="OffersPage-m-right">{text}</p>}
-      <Icon icon="arrow-right" />
-      {text === previousText && <p className="OffersPage-m-left">{text}</p>}
+      { content }
     </Link>
   )
 }
