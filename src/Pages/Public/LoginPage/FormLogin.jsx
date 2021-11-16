f9543e2c29f1effdb80de5d42366961e304055a6
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-16 09:56:49 -0300

    Reformat and useState in form

--- src/Pages/Public/LoginPage/FormLogin.jsx
+++ src/Pages/Public/LoginPage/FormLogin.jsx
@@ -1,15 +1,21 @@
 import TextInput from '../../../Components/TextInput'
 import TextLink from '../../../Components/TextLink'
+import { useState } from 'react'
 
 function FormLogin() {
+    const [selectInput, setSelectInput] = useState(false)
+    const buttonText = 'Continuar'
+    const labelText = 'Telefono, e-mail o usuario'
+    const linkCreateAccount = 'Crear cuenta'
+    const urlRegistration = '/register'
     return (
         <div className="row padding-none ai-start">
             <div className="col w-100 padding-none">
-                <form className=" login-flex fd-col">
-                    <label className="fz-sm m-left-0" htmlFor="login-input">Telefono, e-mail o usuario</label>
-                    <TextInput id="login-input" className={"input rounded p-form m-bottom-2"}></TextInput>
-                    <button className="bg-blue txt-white p-form rounded txt-bold border-none m-bottom-2 m-top-2">Continuar</button>
-                    <TextLink className="txt-center" url={"/register"}>Crear Cuenta</TextLink>
+                <form className="d-flex fd-col">
+                    <label className="fz-sm m-left-0" htmlFor="login-input">{labelText}</label>
+                    <TextInput id="login-input" onFocus={() => setSelectInput(!selectInput)} onBlur={() => setSelectInput(!selectInput)} className={`input round p-form m-bottom-2 ${selectInput ? 'selected' : ''}`}></TextInput>
+                    <button className="bg-blue txt-white p-form round txt-bold border-none m-bottom-2 m-top-2 fz-m">{buttonText}</button>
+                    <TextLink className="txt-center txt-blue" url={urlRegistration}>{linkCreateAccount}</TextLink>
                 </form>
             </div>
         </div>
