d382e2efc789d37c07defbe3232cfa8b29ff8fab
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-16 09:55:29 -0300

    Reformat of login page

--- src/Pages/Public/LoginPage.js
+++ src/Pages/Public/LoginPage.js
@@ -2,31 +2,30 @@
 import DistractionFreeLayout from '../../Layouts/DistractionFreeLayout'
 import FormLogin from './LoginPage/FormLogin'
 import TextLine from '../../Components/TextLine'
-import { Link } from 'react-router-dom'
-function LoginPage() {
-    return (
-        <DistractionFreeLayout>
-            <div className="container">
-                <div className="row jc-center">
-                    <div className="col padding-none fd-col login-flex">
-                        <div className="row padding-none grow-1">
-                            <div className="col padding-none">
-                                <h1 className="title">¡Hola! Ingresá tu teléfono, e‑mail o usuario</h1>
-                                <FormLogin />
-                                {/* <TextLink className="fz-sm txt-center" url="/register" >Necesito ayuda para ingresar</TextLink> */}
-                            </div>
-                        </div>
-                        <div className="row padding-none">
-                            <div className="col padding-none">
-                                <TextLine className="txt-blue fz-sm txt-bold" text="Necesito ayuda para ingresar" />
-                                <div className="txt-center fz-xsm txt-grey-copyright p-0">Protegido por reCAPTCHA-<span className="fz-xsm txt-blue">Privacidad</span>-<span className="fz-xsm txt-blue">Condiciones</span></div>
-                            </div>
-                        </div>
 
-                    </div>
+function LoginPage() {
+
+    const title = '¡Hola! Ingresá tu teléfono, e‑mail o usuario'
+    const reCaptcha = 'Protegido por reCAPTCHA-'
+    const privacy = 'Privacidad'
+    const conditions = 'Condiciones'
+    const needHelp = 'Necesito ayuda para ingresar'
+
+    return (
+        <DistractionFreeLayout className="fd-col">
+            <div className="row grow-1 d-flex ai-start">
+                <div className="col">
+                    <h1 className="title m-top-1 m-bottom-5">{title}</h1>
+                    <FormLogin />
                 </div>
             </div>
-        </DistractionFreeLayout >
+            <div className="row jc-center">
+                <div className="col">
+                    <TextLine className="txt-blue fz-sm txt-bold" text={needHelp} />
+                    <div className="txt-center fz-xsm txt-grey-copyright p-0">{reCaptcha}<span className="fz-xsm txt-blue">{privacy}</span>-<span className="fz-xsm txt-blue">{conditions}</span></div>
+                </div>
+            </div>
+        </DistractionFreeLayout>
     )
 }
 
