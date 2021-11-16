0b9e5bf9878eb3fbe243fbe4f787f422efd1f084
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-16 09:55:05 -0300

    Distraction free layout created

--- src/Layouts/DistractionFreeLayout.js
+++ src/Layouts/DistractionFreeLayout.js
@@ -3,13 +3,27 @@
  * footer: copyright
  */
 
-function DistractionFreeLayout({children}) {
+import TextLine from "../Components/TextLine"
+
+function DistractionFreeLayout({ children, className }) {
+    const footerText = 'Cuidamos tu privacidad'
+    const copyright = 'Copyright © 1999-2021 MercadoLibre S.R.L.'
+
     return (
-        <>
-            <div>Header</div>
-                {children}
-            <div>Footer</div>
-        </>
+        <div className={`d-flex vh-100 ${className}`}>
+            <div className="row h-header bg-primary padding-none">
+                <div className="col">
+                    <a href="/"><img className="logo-footer" src="https://rooftop.dev/images/favicon.png" alt="Rooftop Logo" /> </a>
+                </div>
+            </div>
+            {children}
+            <div className="row br-top d-flex jc-center padding-none">
+                <div className="col">
+                    <TextLine className="order-top-footer fz-sm" text={footerText} />
+                    <TextLine className="order-top-footer fz-xsm txt-grey-copyright" text={copyright} />
+                </div>
+            </div>
+        </div>
     )
 }
 
