b6c6c9536bd77ec8df8a29cbd0148317e7c02b6a
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-16 09:54:19 -0300

    Reformat input text

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -1,9 +1,16 @@
 
-function TextInput({ holderText, className }) {
-    return <input
-        type="text"
-        className={className}
-        placeholder={holderText} />
+function TextInput({ holderText, className, onFocus, onBlur, type = 'text' }) {
+    let types = ['text', 'password', 'number', 'email']
+
+    if (types.includes(type)) {
+        return <input
+            type={type}
+            className={className}
+            placeholder={holderText}
+            onFocus={onFocus}
+            onBlur={onBlur}
+        />
+    }
 }
 
 export default TextInput
