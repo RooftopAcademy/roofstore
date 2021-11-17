801bf45ad0db574be2d365c7f46883b38f39e602
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-17 18:28:13 -0300

    Reformat in form,, now inputs has the useState for changing the styles

--- src/Pages/Public/LoginPage/FormLogin.jsx
+++ src/Pages/Public/LoginPage/FormLogin.jsx
@@ -1,20 +1,20 @@
-import TextInput from '../../../Components/TextInput'
+import FormInput from '../../../Components/FormInput'
 import TextLink from '../../../Components/TextLink'
 import { useState } from 'react'
 
 function FormLogin() {
-    const [selectInput, setSelectInput] = useState(false)
+
     const buttonText = 'Continuar'
     const labelText = 'Telefono, e-mail o usuario'
     const linkCreateAccount = 'Crear cuenta'
     const urlRegistration = '/register'
     return (
-        <div className="row padding-none ai-start">
+        <div className="row padding-none">
             <div className="col w-100 padding-none">
                 <form className="d-flex fd-col">
                     <label className="fz-sm m-left-0" htmlFor="login-input">{labelText}</label>
-                    <TextInput id="login-input" onFocus={() => setSelectInput(!selectInput)} onBlur={() => setSelectInput(!selectInput)} className={`input round p-form m-bottom-2 ${selectInput ? 'selected' : ''}`}></TextInput>
-                    <button className="bg-blue txt-white p-form round txt-bold border-none m-bottom-2 m-top-2 fz-m">{buttonText}</button>
+                    <FormInput id="login-input" type="text" className="input round p-form"></FormInput>
+                    <button className="bg-blue txt-white p-form round txt-bold border-none m-bottom-2 m-top-4 fz-m">{buttonText}</button>
                     <TextLink className="txt-center txt-blue" url={urlRegistration}>{linkCreateAccount}</TextLink>
                 </form>
             </div>
