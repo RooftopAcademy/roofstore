201be3dc947f7e9edfa6374d7f0e329fc6eff3fd
Jora_c <84633047+jocardozo@users.noreply.github.com>
2021-11-18 11:56:28 -0300

    Add Test TextLink

---
+++ src/Components/TextLink.test.js
@@ -0,0 +1,28 @@
+import {render, screen} from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import TextLink from './TextLink';
+
+describe('<TextLink url="/"/>', () => {
+    let component
+    beforeEach(() => {
+        component = render (
+            <BrowserRouter>
+                <TextLink url="/">
+                    Hola
+                </TextLink>
+            </BrowserRouter>
+        )
+    })
+    // Check that url is the one insicated in the argument
+    test("check that url is the one insicated in the argument", async () => {
+        const attributeHrefValue = component.container.querySelector(['a']).attributes['href'].value;
+        expect(attributeHrefValue).toBe('/');
+    });
+
+    // Check that the children is rendered as content between the tags
+    
+    test('render a text ("foo") between anchor tags', async () => {
+        const anchorElement = component.container.querySelector(['a'])
+        expect(anchorElement).toHaveTextContent("Hola")
+    });
+})
