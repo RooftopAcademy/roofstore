9fe6f800fe2c0b9bed65ecbeb996b06e8a273261
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-18 03:58:48 -0300

    Created form for data registration

---
+++ src/Pages/Public/RegisterPages/DataForm.jsx
@@ -0,0 +1,28 @@
+import FormInput from '../../../Components/FormInput'
+
+function DataForm() {
+
+    const buttonText = 'Continuar'
+    const labelName = 'Nombre/s'
+    const labelLastName = 'Apellido/s'
+    const labelDni = 'Documento'
+    const holderDni = 'Escribí solo números, sin puntos ni guiones'
+
+    return (
+        <div className="row padding-none">
+            <div className="col w-100 padding-none">
+                <form className="d-flex fd-col">
+                    <label className="fz-sm m-left-0" htmlFor="name-input">{labelName}</label>
+                    <FormInput id="name-input" type="text" className="input round p-form m-bottom-5"></FormInput>
+                    <label className="fz-sm m-left-0" htmlFor="name-input">{labelLastName}</label>
+                    <FormInput id="lastName-input" type="text" className="input round p-form m-bottom-5"></FormInput>
+                    <label className="fz-sm m-left-0" htmlFor="dni-input">{labelDni}</label>
+                    <FormInput id="dni-input" type="text" holderBottom={holderDni} className="input round p-form "></FormInput>
+                    <button className="bg-blue txt-white p-form round txt-bold border-none fz-m ">{buttonText}</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default DataForm;
