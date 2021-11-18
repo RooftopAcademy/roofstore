c7a48cab052791db0d517b3cac5d5927e96d8f6f
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-18 04:00:15 -0300

    Created requirement for passoword

---
+++ src/Pages/Public/RegisterPages/PasswordRequirement.jsx
@@ -0,0 +1,13 @@
+function PasswordRequirement({ text }) {
+
+    return (
+        <div className="row d-flex padding-none jc-start">
+            <div className={`checkItem m-right-0`}></div>
+            <p className={`fz-sm txt-grey-copyright `}>{text}</p>
+        </div>
+    )
+
+
+}
+
+export default PasswordRequirement;
