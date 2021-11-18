95af4671b5cf2b6ef9d1fed0ed039a0326059b15
Federico L. Martin <fl.martin.dev@gmail.com>
2021-11-18 12:57:39 -0300

    Improving test

--- src/Components/TextLine.test.js
+++ src/Components/TextLine.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import TextLine from "./TextLine";
 
-it("render the text passed in text prop", async () => {
-	render(<TextLine text="Testing TextLine" />);
-	const textElement = screen.getByText("Testing TextLine");
-	expect(textElement).toBeInTheDocument();
-});
+describe("TextLine", () => {
+	beforeEach(() => {
+		render(<TextLine text="Testing TextLine" className="txt-blue" />);
+	});
 
-it("include the classes passed in className prop", async () => {
-	render(<TextLine text="Testing TextLine" className="txt-blue" />);
-	const textElement = screen.getByText("Testing TextLine");
-	expect(textElement).toHaveClass("txt-blue");
+	it("render the text passed in text prop", async () => {
+		const textElement = screen.getByText("Testing TextLine");
+		expect(textElement).toBeInTheDocument();
+	});
+
+	it("include the classes passed in className prop", async () => {
+		const textElement = screen.getByText("Testing TextLine");
+		expect(textElement).toHaveClass("txt-blue");
+	});
 });
