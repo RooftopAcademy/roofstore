54310baacf7d0220d765065e415ffe830f2de728
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-22 14:30:09 -0300

    refactor SeeMore.js

--- src/Pages/Public/ProductPage/Component/SeeMore.js
+++ src/Pages/Public/ProductPage/Component/SeeMore.js
@@ -1,20 +1,24 @@
 import TextLink from "../../../../Components/TextLink";
 import Icon from "../../../../Components/Icon";
 
-function SeeMore({message}) {
-  return (
-    <div class="container">
-      <div class="row grey-border rounded padding-none">
-        <div class="col pading-none ProductPage-font-size-medium">
-        <TextLink  url="/Login" className={"txt-blue"} children={message}/>
+function SeeMore({ message }) {
+    return (
+        <div className="container">
+            <div className="row grey-border rounded padding-none">
+                <div className="col pading-none ProductPage-font-size-medium">
+                    <TextLink
+                        url="/Login"
+                        className={"txt-blue"}
+                        children={message}
+                    />
+                </div>
+                <div className="col">
+                    <span className="txt-blue">
+                        <Icon icon="arrow-right" />
+                    </span>
+                </div>
+            </div>
         </div>
-        <div class="col">
-          <span className="txt-blue">
-            <Icon icon="arrow-right"/>
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
-export default SeeMore
+export default SeeMore;
