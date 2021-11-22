b7f39f14e023a79f3a920f41471b3bad9bcf38bd
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-22 14:31:28 -0300

    refactor Badge.js

--- src/Pages/Public/ProductPage/Component/Badge.js
+++ src/Pages/Public/ProductPage/Component/Badge.js
@@ -2,16 +2,16 @@ import Icon from "../../../../Components/Icon"
 import TextLine from "../../../../Components/TextLine";
 import TextLink from "../../../../Components/TextLink";
 
-function Badge({icon, title, description, link}) {
+function Badge({icon, title, description, txtLink}) {
     return(
-        <div class="row padding-none ProductPage-jc-start m-left-0">
-            <div class="col ProductPage-as-baseline">
+        <div className="row padding-none ProductPage-jc-start m-left-0">
+            <div className="col ProductPage-as-baseline">
                 <span className="txt-green"><Icon icon={icon}/></span>
             </div>
             <div className="col">
                 <TextLine text={title} className={"txt-green"}/>  
                 <TextLine text={description} className={"ProductPage-txt-light-grey ProductPage-font-size-medium"}/> 
-                <TextLink  url="/Login" className={"txt-blue ProductPage-font-size-medium"} children={link}/>
+                <TextLink  url="/Login" className={"txt-blue ProductPage-font-size-medium"} children={txtLink}/>
             </div> 
         </div>
     );
