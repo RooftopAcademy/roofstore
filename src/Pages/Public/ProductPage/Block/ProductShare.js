5c354fade9f47b51690b1f17c711986f21f9d6fd
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-23 12:07:56 -0300

    refactor src/Pages/Public/ProductPage/Block/ProductShare.js

--- src/Pages/Public/ProductPage/Block/ProductShare.js
+++ src/Pages/Public/ProductPage/Block/ProductShare.js
@@ -6,11 +6,11 @@ function ProductShare() {
             <div className="row padding-none ProductPage-jc-center">
                 <div className="col ProductPage-d-flex">
                     <span className="m-right-0 txt-blue"><Icon icon="empty-heart"/></span>
-                    <span><TextLine text="Agregar a favoritos" className={"txt-blue ProductPage-font-size-medium"}/></span>
+                    <span><TextLine text="Agregar a favoritos" className={"txt-blue ProductPage-fs-14"}/></span>
                 </div>
                 <div className="col ProductPage-d-flex">
                     <span className="m-right-0 txt-blue"><Icon icon="share"/></span>
-                    <span><TextLine text="Compartir" className={"txt-blue ProductPage-font-size-medium"}/></span>
+                    <span><TextLine text="Compartir" className={"txt-blue ProductPage-fs-14"}/></span>
                 </div>
             </div>
 
