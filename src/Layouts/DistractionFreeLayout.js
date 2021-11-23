6d1fe871384910d8afba0d8e98dbfe7f4676d7a4
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-23 10:27:10 -0300

    Inprove on footer DistractionFree

--- src/Layouts/DistractionFreeLayout.js
+++ src/Layouts/DistractionFreeLayout.js
@@ -5,25 +5,29 @@
 
 import TextLine from "../Components/TextLine"
 
-function DistractionFreeLayout({ children, className, isFooter }) {
+function DistractionFreeLayout({ children, isFooter }) {
     const footerText = 'Cuidamos tu privacidad'
     const copyright = 'Copyright © 1999-2021 MercadoLibre S.R.L.'
     const logoUrl = 'https://rooftop.dev/images/favicon.png'
     const logoAlt = 'Rooftop Logo'
     return (
-        <div className={`d-flex vh-100 ${className}`}>
+        <div className="d-flex vh-100 fd-col">
             <div className="row h-header bg-primary padding-none">
                 <div className="col">
                     <a href="/"><img className="logo-footer" src={logoUrl} alt={logoAlt} /> </a>
                 </div>
             </div>
             {children}
-            <div className={`row br-top d-flex jc-center padding-none ${!(isFooter) ? 'd-none' : ''}`}>
-                <div className="col">
-                    <TextLine className="order-top-footer fz-sm" text={footerText} />
-                    <TextLine className="order-top-footer fz-xsm txt-grey-copyright" text={copyright} />
+            {isFooter ?
+                <div className="row br-top d-flex jc-center padding-none">
+                    <div className="col">
+                        <TextLine className="order-top-footer fz-sm" text={footerText} />
+                        <TextLine className="order-top-footer fz-xsm txt-grey-copyright" text={copyright} />
+                    </div>
                 </div>
-            </div>
+                :
+                null
+            }
         </div>
     )
 }
