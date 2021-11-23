8d620cc1ce5ef3178068ec0e28ef35ddc6b75113
Fernando Alba <fernando.alba@live.com.ar>
2021-11-23 16:03:16 -0300

    fix: add key property in Carrousel component

--- src/Pages/HomePage/Carrousel.js
+++ src/Pages/HomePage/Carrousel.js
@@ -6,7 +6,7 @@ function Carrousel({bannerList}) {
             <div className="col HomePage-col-12 padding-none HomePage-d-flex overflow-scrollx">
                 {bannerList.map(item => {
                     return (
-                        <div className="HomePage-col-12">
+                        <div className="HomePage-col-12" key={item.id}>
                             <Link to={item.url} key={item.id}>
                                 <img className="HomePage-img" src={item.image} alt={item.title} />
                             </Link>
