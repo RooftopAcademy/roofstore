2087c991b22942ce1eae78b325384517df63274f
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-23 12:09:23 -0300

    refactor src/Pages/Public/ProductPage/Component/Badge.js

--- src/Pages/Public/ProductPage/Component/Badge.js
+++ src/Pages/Public/ProductPage/Component/Badge.js
@@ -2,19 +2,20 @@ import Icon from "../../../../Components/Icon"
 import TextLine from "../../../../Components/TextLine";
 import TextLink from "../../../../Components/TextLink";
 
-function Badge({icon, title, description, txtLink}) {
+function Badge({icon, title, description, txtLink, url}) {
     return(
-        <div className="row padding-none ProductPage-jc-start m-left-0">
-            <div className="col ProductPage-as-baseline">
-                <span className="txt-green"><Icon icon={icon}/></span>
+            <div className="row p-0 ProductPage-jc-start">
+                <div className="col ProductPage-as-baseline">
+                    <span className="txt-green"><Icon icon={icon}/></span>
+                </div>
+                <div className="col">
+                    <TextLine text={title} className={"txt-green"}/>  
+                    <TextLine text={description} className={"ProductPage-txt-light-grey ProductPage-fs-14"}/> 
+                    <TextLink  url={url} className={"txt-blue ProductPage-fs-14"} children={txtLink}/>
+                </div> 
             </div>
-            <div className="col">
-                <TextLine text={title} className={"txt-green"}/>  
-                <TextLine text={description} className={"ProductPage-txt-light-grey ProductPage-font-size-medium"}/> 
-                <TextLink  url="/Login" className={"txt-blue ProductPage-font-size-medium"} children={txtLink}/>
-            </div> 
-        </div>
-    );
+    
+        );
 }
 
 export default Badge
