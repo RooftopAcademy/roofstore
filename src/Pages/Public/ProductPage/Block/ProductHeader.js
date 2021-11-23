a646e3a732380592a0464e896f6381f938259985
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-23 12:00:06 -0300

    refactor src/Pages/Public/ProductPage/Block/ProductHeader.js

--- src/Pages/Public/ProductPage/Block/ProductHeader.js
+++ src/Pages/Public/ProductPage/Block/ProductHeader.js
@@ -11,28 +11,30 @@ function ProductHeader({item, store}) {
     let por = "Por "
     let textTag = "MÁS VENDIDO"
     let category = item.ranking + "° en " + item.category
+    let urlStore = "/profile/mall-360"
+    let urlCategory = "/category/bicicletas"
 
 
     return (
         <div>
             <div className="row navbar-p-base">
                 <div className="col padding-none m-left-0">
-                    <TextLine text={subtitle} className={"ProductPage-txt-light-grey ProductPage-font-size-subtitle"}/>
+                    <TextLine text={subtitle} className={"ProductPage-txt-light-grey ProductPage-fs-12"}/>
                 </div>
             </div>
             <div className="row navbar-p-base ProductPage-p-bottom-0 ProductPage-p-top-0">
                 <div className="col padding-none m-left-0">
-                        <TextLine text={item.title} className={"txt-black ProductPage-font-size-title"}/>
+                        <TextLine text={item.title} className={"txt-black ProductPage-fs-16"}/>
                 </div>
             </div>
             <div className="row padding-none">
                 <div className="col padding-none m-left-3">
                     {store.oficialStore &&
-                        <span className="ProductPage-d-flex ProductPage-font-size-subtitle">
+                        <span className="ProductPage-d-flex ProductPage-fs-12">
                             <span className="">
                                 <TextLine text={por} className={"ProductPage-txt-light-grey"}/>
                             </span>
-                            <TextLink  url="/Login" className={"txt-blue"} children={store.nameStore}/>
+                            <TextLink  url={urlStore} className={"txt-blue"} children={store.nameStore}/>
                         </span>
                     }   
                     
@@ -40,8 +42,8 @@ function ProductHeader({item, store}) {
             </div>
             <div className="row navbar-p-base">
                 <div className="col padding-none m-left-0">
-                    <div className="ProductPage-d-flex  ProductPage-font-size-medium"> 
-                        <span className="txt-blue m-right-0 ProductPage-font-size-small">
+                    <div className="ProductPage-d-flex  ProductPage-fs-14"> 
+                        <span className="txt-blue m-right-0 ProductPage-fs-10">
                             <Icon icon="full-star"/>
                             <Icon icon="full-star"/>
                             <Icon icon="full-star"/>
@@ -61,7 +63,7 @@ function ProductHeader({item, store}) {
                             <span className="m-right-0"> 
                                 <TextTag text={textTag} txtColor="txt-white" bgColor="ProductPage-bg-orange"/>
                             </span> 
-                            <TextLink  url="/Login" className={"txt-blue ProductPage-font-size-subtitle"} children={category}/>
+                            <TextLink  url={urlCategory} className={"txt-blue ProductPage-fs-12"} children={category}/>
                         </span>
                     }
                 </div>
