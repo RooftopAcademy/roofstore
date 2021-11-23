e5db19ebe31f4c68e0a194c6992c46c257e86f53
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-23 09:53:22 -0300

    DataPage its rendered on RegisterPage

--- src/Pages/Public/RegisterPages/DataPage.jsx
+++ src/Pages/Public/RegisterPages/DataPage.jsx
@@ -1,21 +1,18 @@
 import DataForm from "./DataForm";
-import DistractionFreeLayout from "../../../Layouts/DistractionFreeLayout";
 
-function DataPage() {
+function DataPage({ optionActive, setOptionActive, className }) {
 
     const title = '¿Cuál es tu nombre completo y DNI?'
     const explication = 'Completa estos datos tal cual figuran en tu documento de identidad.'
 
     return (
-        <DistractionFreeLayout className="fd-col" isFooter={false}>
-            <div className="row grow-1 d-flex ai-start">
-                <div className="col">
-                    <h1 className="title m-top-1 m-bottom-1">{title}</h1>
-                    <p className="fz-sm txt-grey-copyright m-bottom-5">{explication}</p>
-                    <DataForm />
-                </div>
+        <div className={`row grow-1 d-flex ai-start ${className}`}>
+            <div className="col">
+                <h1 className="title m-top-1 m-bottom-1">{title}</h1>
+                <p className="fz-sm txt-grey-copyright m-bottom-5">{explication}</p>
+                <DataForm setOptionActive={setOptionActive} optionActive={optionActive} />
             </div>
-        </DistractionFreeLayout>
+        </div>
     )
 }
 export default DataPage;
