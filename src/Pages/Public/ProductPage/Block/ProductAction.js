8e0ce87cca42a9db5f78b24abb7eb72feab8d24e
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-23 11:57:53 -0300

    refactor src/Pages/Public/ProductPage/Block/ProductAction.js

--- src/Pages/Public/ProductPage/Block/ProductAction.js
+++ src/Pages/Public/ProductPage/Block/ProductAction.js
@@ -4,46 +4,48 @@ import product from "../dataProduct"
 
 let buttonShop = "Comprar ahora" 
 let buttonAdd = "Agregar al carrito"
-function ProductAction() {
+
+function ProductAction({item}) {
     return (
-        <div className="container">
-            <div className="row">
+            <div className="row p-0 ">
                 <div className="col padding-none ProductPage-w-100">
                     <div className="ProductPage-p-bottom-2">
                         <button className=" bg-blue txt-white rounded txt-bold ProductPage-border-none ProductPage-w-100">
-                            <TextLine text={buttonShop} className={"ProductPage-p-button ProductPage-font-size-title"}/>
+                            <TextLine text={buttonShop} className={"ProductPage-p-button ProductPage-fs-16"}/>
                         </button>
                     </div>
                     <div>
-                        <button className="bg-light-blue txt-blue rounded txt-bold ProductPage-border-none ProductPage-w-100">
-                            <TextLine text={buttonAdd} className={"ProductPage-p-button ProductPage-font-size-title"}/>
+                        <button className="bg-light-blue txt-blue rounded txt-bold ProductPage-border-none ProductPage-w-100 ">
+                            <TextLine text={buttonAdd} className={"ProductPage-p-button ProductPage-fs-16"}/>
                         </button>
                     <Benefit 
                         icon = "devolution"
                         txtLink = "Devolución gratis. "
-                        url = "#" 
+                        url = "/politicas-de-devolucion" 
                         description = "Tenés 30 días desde que lo recibís."
                     />
                     <Benefit 
                         icon = "shield"
                         txtLink = "Compra protegida. "
-                        url = "#" 
+                        url = "/compra-protegida" 
                         description = "recibí el producto que esperabas o te devolvemos tu dinero."
                     />
                     <Benefit 
                         icon = "trophy"
                         txtLink = "Mercado Puntos. "
-                        url = "#" 
-                        description = {"Sumas " + product.points + " puntos"}
+                        url = "/mercado-puntos" 
+                        description = {"Sumás " + product.points + " puntos"}
                     />
-                    <Benefit 
-                        icon = "medal"
-                        description = {"6 meses de garantia de fábrica"}
-                    />
+                    {item.assurance &&
+                        <Benefit 
+                            icon = "medal"
+                            description = {product.insuranceMonths + " meses de garantia de fábrica"}
+                        />
+                    }
                     </div>
                 </div>    
             </div>
-        </div>
+     
     );
 }
 export default ProductAction
