369d9ce648b598d5551143a9f3509a7bd30d99be
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-23 09:54:16 -0300

    EmailPage its rendered on RegisterPage

--- src/Pages/Public/RegisterPages/EmailPage.jsx
+++ src/Pages/Public/RegisterPages/EmailPage.jsx
@@ -1,26 +1,24 @@
 
-import DistractionFreeLayout from "../../../Layouts/DistractionFreeLayout";
 import FormInput from "../../../Components/FormInput";
 
 
-function EmailPage() {
+
+function EmailPage({ optionActive, className, setOptionActive }) {
 
     const title = 'Ingresá tu e-mail'
     const explication = 'Te enviaremos un mensaje para confirmarlo.'
     const buttonText = 'Enviar e-mail de confirmación'
     return (
-        <DistractionFreeLayout className="fd-col" isFooter={false}>
-            <div className="row">
-                <div className="col w-100">
-                    <h1 className="title m-top-1 m-bottom-2">{title}</h1>
-                    <p className="fz-sm txt-grey-copyright m-bottom-5">{explication}</p>
-                    <form className="d-flex fd-col">
-                        <FormInput id="name-input" type="email" className="input round p-form m-bottom-5" />
-                        <button className=" w-100 bg-blue txt-white p-form round txt-bold border-none fz-m">{buttonText}</button>
-                    </form>
-                </div>
+        <div className={`row ${className}`}>
+            <div className="col w-100">
+                <h1 className="title m-top-1 m-bottom-2">{title}</h1>
+                <p className="fz-sm txt-grey-copyright m-bottom-5">{explication}</p>
+                <form className="d-flex fd-col">
+                    <FormInput id="email-input" type="email" className="input round p-form m-bottom-5" />
+                    <button type="button" onClick={() => { setOptionActive(optionActive++) }} className=" w-100 bg-blue txt-white p-form round txt-bold border-none fz-m">{buttonText}</button>
+                </form>
             </div>
-        </DistractionFreeLayout>
+        </div>
     )
 }
 export default EmailPage;
