bef1e8fe2cda0cec2f1e95bf4da3aa1a24cd0a20
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-23 12:08:12 -0300

    refactor src/Pages/Public/ProductPage/Block/ProductStock.js

--- src/Pages/Public/ProductPage/Block/ProductStock.js
+++ src/Pages/Public/ProductPage/Block/ProductStock.js
@@ -3,39 +3,46 @@ import Selector from "../Component/Selector";
 import product from "../dataProduct";
 
 let title = "Stock disponible"
-let jj = "¡Ultimo disponible!"
+let message = "¡Ultimo disponible!"
 let ph = "(" + product.stock + " disponibles)"
-
+let content = ""
 
 
 function ProductStock({item}) {
-    return (
-        <div className="container">
-            {(item.stock > 1) &&
-                <div>
-                    <div className="row padding-none">
-                        <div className="col">
-                            <TextLine text={title} className={"txt-black ProductPage-font-size-large"}/>
-                        </div>
-                    </div>
-                    <Selector
-                        variantOpcion = "Stock: "
-                        opcion = "1"
-                        placeHolder = {ph}
-                       
-                    />
+    if(item.stock > 1) {
+    
+        content = (
+           
+            <div>
+            <div className="row padding-none">
+                <div className="col">
+                    <TextLine text={title} className={"txt-black ProductPage-fs-18"}/>
                 </div>
-                
-            }{
-                <div className="row padding-none">
-                        <div className="col">
-                            <TextLine text={jj} className={"ProductPage-font-size-large"}/>
-                        </div>
-                    </div>
-            }
-                
+            </div>
+            <Selector
+                variantOpcion = "Stock: "
+                opcion = "1"
+                placeHolder = {ph}
+               
+            />
         </div>
+        )
         
+    }else {
+        content = (
+            <div className="row padding-none">
+                <div className="col">
+                    <TextLine text={message} className={"ProductPage-fs-18"}/>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <span>
+            {content}
+            
+        </span>
     );
 }
 export default ProductStock
