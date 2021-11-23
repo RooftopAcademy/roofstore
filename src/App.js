0fea744bf899899cf4adf2de93cb67373fc93616
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-23 10:30:40 -0300

    No need of router, pages are now rendered on RegisterPage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/Public/HomePage';
 import LoginPage from './Pages/Public/LoginPage';
 import RegisterPage from './Pages/Public/RegisterPage';
-import DataPage from './Pages/Public/RegisterPages/DataPage';
-import EmailPage from './Pages/Public/RegisterPages/EmailPage';
-import PasswordPage from './Pages/Public/RegisterPages/PasswordPage';
-
 
 
 function App() {
@@ -16,9 +12,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/register/email" element={<EmailPage />} />
-        <Route path="/register/data" element={<DataPage />} />
-        <Route path="/register/pass" element={<PasswordPage />} />
       </Routes>
     </BrowserRouter>
   );
