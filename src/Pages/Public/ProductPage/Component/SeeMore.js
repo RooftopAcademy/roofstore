580b29c7cb4e8c34e4d6ef729396607bc4221809
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-24 11:00:55 -0300

    added new feature: url redirect component

--- src/Pages/Public/ProductPage/Component/SeeMore.js
+++ src/Pages/Public/ProductPage/Component/SeeMore.js
@@ -1,24 +1,27 @@
+import { Link } from "react-router-dom"
 import TextLink from "../../../../Components/TextLink";
 import Icon from "../../../../Components/Icon";
 
 function SeeMore({ message, url }) {
     return (
-        <div className="container">
-            <div className="row grey-border rounded padding-none">
-                <div className="col pading-none ProductPage-fs-14">
-                    <TextLink
-                        url={url}
-                        className={"txt-blue"}
-                        children={message}
-                    />
-                </div>
-                <div className="col">
-                    <span className="txt-blue">
-                        <Icon icon="arrow-right" />
-                    </span>
+        <Link className={'link'} to={url}>
+            <div className="container">
+                <div className="row grey-border rounded padding-none">
+                    <div className="col pading-none ProductPage-fs-14">
+                        <TextLink
+                            url={url}
+                            className={"txt-blue"}
+                            children={message}
+                        />
+                    </div>
+                    <div className="col">
+                        <span className="txt-blue">
+                            <Icon icon="arrow-right" />
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
 export default SeeMore;
