a6e508073e21882617e2775cf3aa80211e66741b
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-24 11:04:31 -0300

    updated store information

--- src/Pages/Public/ProductPage/dataStore.js
+++ src/Pages/Public/ProductPage/dataStore.js
@@ -1,10 +1,7 @@
 const store = {
-    'SalesForMonth' : 1520,
+    'salesInTheLastMonth' : 18748,
     'nameStore' : "Mall 360",
     'logoStore' : "https://http2.mlstatic.com/D_Q_NP_980269-MLA26700254444_012018-G.jpg",
     'oficialStore' : true,
-    'freeShipping' : false,
-    'reputation' : [1,2,3,4,5]  // 0 = Mala, 5 = Excelente
-
 }
 export default store
