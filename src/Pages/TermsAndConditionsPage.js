6fdd99eee0dcf5eb8bc53cb4a0f25f5654117dc4
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-26 15:43:17 -0300

    created terms and contition page view

--- src/Pages/TermsAndConditionsPage.js
+++ src/Pages/TermsAndConditionsPage.js
@@ -1,5 +1,25 @@
+import DistractionFreeLayout from "../Layouts/DistractionFreeLayout";
+import info from "./TermsAndConditionsPage/info";
+
+let titulo = "Términos y condiciones de uso del Sitio"
+let text = info
 function TermsAndConditionsPage() {
-    return 
+    return(
+        <DistractionFreeLayout>
+            <div className="container bg-light-grey">
+                <div className="row txt-bold ProductPage-fs-24">
+                    <div className="col">
+                        {titulo}
+                    </div>
+                </div>
+                <div className="row bg-white">
+                    <div className="col ">
+                        {text}
+                    </div>
+                </div>
+            </div>
+        </DistractionFreeLayout>
+    ) 
   }
   
   export default TermsAndConditionsPage;
