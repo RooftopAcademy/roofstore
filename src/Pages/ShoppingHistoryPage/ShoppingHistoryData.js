2fa6ba4e86f0c15ce4d25e17c991d7f8d50b6407
Scarlet <smichelle05a@gmail.com>
2021-11-26 17:59:59 -0300

    add id to each product

--- src/Pages/ShoppingHistoryPage/ShoppingHistoryData.js
+++ src/Pages/ShoppingHistoryPage/ShoppingHistoryData.js
@@ -1,23 +1,27 @@
-const shoppingHistoryData = [
+const ShoppingHistoryData = [
   {
+    id: 1,
     shoppingDate: '14 de Octubre',
     img: 'https://http2.mlstatic.com/D_776646-MLA47291405232_082021-I.jpg',
     shippingStatus: 'En camino',
     deliveryDate: 'Tu compra llega el 16 de Octubre'
   },
   {
+    id: 2,
     shoppingDate: '21 de Septiembre',
     img: 'https://http2.mlstatic.com/D_624116-MLA44023587000_112020-I.jpg',
     shippingStatus: 'Entregado',
     deliveryDate: 'Llegó el 22 de Septiembre'
   },
   {
+    id: 3,
     shoppingDate: '19 de Septiembre',
     img: 'https://http2.mlstatic.com/D_621066-MLA48187015333_112021-I.jpg',
     shippingStatus: 'Entregado',
     deliveryDate: 'Llegó el 20 de Septiembre'
   },
   {
+    id: 4,
     shoppingDate: '14 de Octubre',
     img: 'https://http2.mlstatic.com/D_915319-MLA31114614647_062019-I.jpg',
     shippingStatus: 'Entregado',
@@ -25,4 +29,4 @@ const shoppingHistoryData = [
   },
 ]
 
-export default shoppingHistoryData;
+export default ShoppingHistoryData;
