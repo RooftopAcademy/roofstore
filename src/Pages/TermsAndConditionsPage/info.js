af6629b806cc7863390fb9b656b1a86d219e8331
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-26 15:40:39 -0300

    added new file src/Pages/TermsAndConditionsPage/info.js

---
+++ src/Pages/TermsAndConditionsPage/info.js
@@ -0,0 +1,9 @@
+const info = 
+<div className="">
+    <p>Versión vigente 04/10/2021</p>
+    <p>Este contrato describe los términos y condiciones generales (los "Términos y Condiciones Generales") aplicables al uso de los servicios ofrecidos por Mercado Libre S.R.L., CUIT 30-70308853-4, ("los Servicios") dentro de los sitios www.mercadolibre.com.ar y www.mercadopago.com.ar ("MercadoLibre" o el "sitio"). Cualquier persona (en adelante "Usuario" o en plural "Usuarios") que desee acceder y/o usar el sitio o los Servicios podrá hacerlo sujetándose a los Términos y Condiciones Generales respectivos, junto con todas las demás políticas y principios que rigen Mercado Libre y que son incorporados al presente por referencia.</p>
+    <p><strong>CUALQUIER PERSONA QUE NO ACEPTE ESTOS TÉRMINOS Y CONDICIONES GENERALES, LOS CUALES TIENEN UN CARÁCTER OBLIGATORIO Y VINCULANTE, DEBERÁ ABSTENERSE DE UTILIZAR EL SITIO Y/O LOS SERVICIOS.</strong></p>
+    
+</div>
+
+export default info
