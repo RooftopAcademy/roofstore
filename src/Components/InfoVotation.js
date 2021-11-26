f4e3cb8e06128dd9e53468115acc2490c1870ab1
Federico L. Martin <fl.martin.dev@gmail.com>
2021-11-26 16:39:52 -0300

    Styling buttons

--- src/Components/InfoVotation.js
+++ src/Components/InfoVotation.js
@@ -2,18 +2,20 @@ import TextLine from "./TextLine";
 
 function InfoVotation() {
 	return (
-		<div className="rounded container shadow-m">
-			<TextLine
-				text="¿Te fue útil la información?"
-				className="m-top-5 MenuMobile-m-base-left Footer-txt-start title txt-black "
-			/>
-			<div className="row ">
-				<button className="rounded navbar-img-log border-none MenuMobile-m-base-right InfoVotation-bg-light-blue txt-bold InfoVotation-txt-blue bg-light-blue">
-					Si
-				</button>
-				<button className="rounded navbar-img-log border-none InfoVotation-bg-light-blue txt-bold InfoVotation-txt-blue bg-light-blue">
-					No
-				</button>
+		<div className="p-0 bg-light-grey">
+			<div className="rounded container shadow-sm bg-white p-0">
+				<TextLine
+					text="¿Te fue útil la información?"
+					className="MenuMobile-p-0-vertical m-bottom-0 Footer-txt-start title txt-black "
+				/>
+				<div className="row MenuMobile-p-0-vertical">
+					<button className="rounded MenuMobile-btn border-none MenuMobile-m-base-right InfoVotation-bg-light-blue txt-bold InfoVotation-txt-blue">
+						Si
+					</button>
+					<button className="rounded MenuMobile-btn border-none InfoVotation-bg-light-blue txt-bold InfoVotation-txt-blue">
+						No
+					</button>
+				</div>
 			</div>
 		</div>
 	);
