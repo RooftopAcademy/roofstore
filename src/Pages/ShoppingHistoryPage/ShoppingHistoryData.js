dfb0988cfb5bae11f928c6ee9a97631698c6abfd
Scarlet <smichelle05a@gmail.com>
2021-11-26 16:49:13 -0300

    add shopping history data

---
+++ src/Pages/ShoppingHistoryPage/ShoppingHistoryData.js
@@ -0,0 +1,28 @@
+const shoppingHistoryData = [
+  {
+    shoppingDate: '14 de Octubre',
+    img: 'https://http2.mlstatic.com/D_776646-MLA47291405232_082021-I.jpg',
+    shippingStatus: 'En camino',
+    deliveryDate: 'Tu compra llega el 16 de Octubre'
+  },
+  {
+    shoppingDate: '21 de Septiembre',
+    img: 'https://http2.mlstatic.com/D_624116-MLA44023587000_112020-I.jpg',
+    shippingStatus: 'Entregado',
+    deliveryDate: 'Llegó el 22 de Septiembre'
+  },
+  {
+    shoppingDate: '19 de Septiembre',
+    img: 'https://http2.mlstatic.com/D_621066-MLA48187015333_112021-I.jpg',
+    shippingStatus: 'Entregado',
+    deliveryDate: 'Llegó el 20 de Septiembre'
+  },
+  {
+    shoppingDate: '14 de Octubre',
+    img: 'https://http2.mlstatic.com/D_915319-MLA31114614647_062019-I.jpg',
+    shippingStatus: 'Entregado',
+    deliveryDate: 'Llegó el 20 de Septiembre'
+  },
+]
+
+export default shoppingHistoryData;
