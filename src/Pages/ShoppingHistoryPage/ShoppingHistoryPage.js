68e2252e7e90d3bdeea68efa3888c83504d65c71
Scarlet <smichelle05a@gmail.com>
2021-11-26 18:28:09 -0300

    finish component

--- src/Pages/ShoppingHistoryPage/ShoppingHistoryPage.js
+++ src/Pages/ShoppingHistoryPage/ShoppingHistoryPage.js
@@ -1,21 +1,31 @@
-// import { Link } from "react-router-dom"
-import TextLine from '../../Components/TextLine'
 import TextLink from '../../Components/TextLink'
+import ShoppingHistoryData from './ShoppingHistoryData'
+import ShoppingHistoryItem from './shoppingHistoryItem'
+import Pagination from '../Public/OffersPage/Blocks/Pagination'
 
-function shoppingHistoryPage({ shoppingHistoryData }) {
+function shoppingHistoryPage() {
   const title = 'Mis Compras'
   const navigationIcon = 'fas fa-arrow-left txt-white'
+  const data = ShoppingHistoryData
+
   return (
-    <div className="d-flex vh-100 fd-col">
-      <div className="row h-header bg-primary padding-none txt-white">
-        <div className="col d-flex">
-          <TextLink url="/">
-            <i className={navigationIcon}></i>
-          </TextLink>
-          <p className="txt-white">{title}</p>
+    <div className="d-flex vh-100 fd-col bg-main">
+        <div className="row h-header bg-primary padding-none txt-white">
+          <div className="col d-flex w-100">
+            <TextLink url="/">
+              <i className={navigationIcon}></i>
+            </TextLink>
+            <p className="txt-white grow-1 m-right-1 txt-center">{title}</p>
+          </div>
         </div>
+      <div className="container p-none grow-1">
+        {data.map((item) => {
+          return <ShoppingHistoryItem shoppingData={item} key={item.id} />
+        })}
       </div>
-      AQUI VA OTRA COSA
+      <footer>
+        <Pagination numPages='2'/>
+      </footer>
     </div>
   )
 }
