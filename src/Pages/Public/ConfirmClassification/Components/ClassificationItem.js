1696721eba426e299a8f15731b8e09a782da7c2a
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-27 17:32:36 -0300

    Added a class for test uses

--- src/Pages/Public/ConfirmClassification/Components/ClassificationItem.js
+++ src/Pages/Public/ConfirmClassification/Components/ClassificationItem.js
@@ -5,7 +5,7 @@ export default function ClassificationItem({ data: { name }, num }) {
   const isSubSubClassif = num > 1 ? 'ConfirmClassification-m-6-left' : '';
 
   return (
-    <div className={`col d-flex padding-none OffersPage-m-bottom ${isSubClassif} ${isSubSubClassif}`}>
+    <div className={`col d-flex padding-none OffersPage-m-bottom ${isSubClassif} ${isSubSubClassif} js-classification`}>
       { folderIcon() }
       <p className="OffersPage-p-0-left ConfirmClassification-txt-mb txt-grey">{ name }</p>
     </div>
