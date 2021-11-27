a3e9ee181cf8f46ca674f7573693ba3eca928aac
Kevin Dev <kevin.developer71@gmail.com>
2021-11-27 11:23:37 -0600

    Se crea la pagina de DefensaConsumidor

--- src/Pages/Public/DefensaConsumidor/DefensaConsumidorItem.js
+++ src/Pages/Public/DefensaConsumidor/DefensaConsumidorItem.js
@@ -1,17 +1,18 @@
-import React from 'react'
-
 function DefensaConsumidorItem({ title, children, lastModifiedDate }) {
   return (
     <section className="border-top-footer">
-      <div className="row DefensaConsumidor-flex-end">
+      <div className="row DefensaConsumidor-flex-end padding-none">
         <button className="link-color ProductPage-keyword-information-button DefensaConsumidor-bg-transparent">
           Volver arriba
         </button>
       </div>
 
-      <h3>{title}</h3>
-      {lastModifiedDate && <p>Última modificación: {lastModifiedDate}</p>}
-      <div>{children}</div>
+      <h4 className="DefensaConsumidor-subtitle m-left-5">{title}</h4>
+      {lastModifiedDate && <p className="m-left-5">Última modificación: {lastModifiedDate}</p>}
+      <div>&nbsp;</div>
+      <div className="DefensaConsumidor-section">
+        {children}
+      </div>
     </section>
   )
 }
