5c2205db1b3e9a8536b8a55c55508e2e6c299c31
Scarlet <smichelle05a@gmail.com>
2021-11-27 14:50:56 -0300

    add class to identify card as shopping item

--- src/Pages/ShoppingHistoryPage/shoppingHistoryItem.js
+++ src/Pages/ShoppingHistoryPage/shoppingHistoryItem.js
@@ -1,7 +1,7 @@
 function ShoppingHistoryItem({shoppingData}) {
   const data = shoppingData
   return (
-    <div className="row">
+    <div className="row shopping-history-item">
         <div className="col bg-white rounded shadow-sm padding-none d-flex w-100 fd-col">
           <p className="txt-black txt-bold br-btm p-0">
             {data.shoppingDate}
