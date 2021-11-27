c3530742fb6f7122ba2e932b20bca51b4f12f618
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-27 20:35:09 -0300

    Added ConfirmClassification page in '/sell/confirm-classification' route in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './Pages/Public/LoginPage';
 import RegisterPage from './Pages/Public/RegisterPage';
 import OffersPage from './Pages/Public/OffersPage';
 import ProductPage from './Pages/Public/ProductPage/ProductPage';
+import ConfirmClassification from './Pages/Public/ConfirmClassification';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/offers" element={<OffersPage />} />
         <Route path="/product" element={<ProductPage />} />
+        <Route path="/sell/confirm-classification" element={<ConfirmClassification />} />
       </Routes>
     </BrowserRouter>
   );
