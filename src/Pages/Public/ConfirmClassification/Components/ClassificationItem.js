ba7b8323331265432ac2e565f0ef023540044aa7
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-27 17:08:21 -0300

    Added classification Data, svg icons and ClassificationItems component for ConfirmClassification page

---
+++ src/Pages/Public/ConfirmClassification/Components/ClassificationItem.js
@@ -0,0 +1,13 @@
+import { folderIcon } from "../svgIcon";
+
+export default function ClassificationItem({ data: { name }, num }) {
+  const isSubClassif = num ? 'ConfirmClassification-m-3-left' : '';
+  const isSubSubClassif = num > 1 ? 'ConfirmClassification-m-6-left' : '';
+
+  return (
+    <div className={`col d-flex padding-none OffersPage-m-bottom ${isSubClassif} ${isSubSubClassif}`}>
+      { folderIcon() }
+      <p className="OffersPage-p-0-left ConfirmClassification-txt-mb txt-grey">{ name }</p>
+    </div>
+  )
+}
---
+++ src/Pages/Public/ConfirmClassification/svgIcon.js
@@ -0,0 +1,16 @@
+export function arrowLeftIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+      <path d="M6 15.667l9 9L16.333 23 10 16.667h16v-2H10l6.333-6.333L15 6.667z" fill="#444"/>
+    </svg>
+  )
+}
+
+export function folderIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 46 43" fill="none">
+      <rect x="1" y="8.84705" width="44" height="33.1529" rx="5" stroke="#4a4a4a" strokeWidth="3"/>
+      <path d="M1 14.6824V5.5C1 3.29086 2.79086 1.5 5 1.5H15.5C17.7091 1.5 19.5 3.29086 19.5 5.5V8.82353" stroke="#4a4a4a" strokeWidth="3"/>
+    </svg>
+  )
+}
