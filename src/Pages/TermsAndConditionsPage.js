61d000954b16503961fe247296fc4e4f0e326c5a
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-27 21:10:28 -0300

    updated the information on the privacy policy page

--- src/Pages/TermsAndConditionsPage.js
+++ src/Pages/TermsAndConditionsPage.js
@@ -6,13 +6,13 @@ let text = info
 function TermsAndConditionsPage() {
     return(
         <DistractionFreeLayout>
-            <div className="container bg-light-grey">
+            <div className="container bg-light-grey ">
                 <div className="row txt-bold ProductPage-fs-24">
                     <div className="col">
                         {titulo}
                     </div>
                 </div>
-                <div className="row bg-white">
+                <div className="row bg-white rounded">
                     <div className="col ">
                         {text}
                     </div>
