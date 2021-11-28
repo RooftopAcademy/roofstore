dc649728e040f8a62ec21c2ff40918cfcb3b7dff
Mauri548 <mauricioferreyra548@gmail.com>
2021-11-28 00:39:22 -0300

    created view for page "que queres publicar"

---
+++ src/Pages/PublishingProcess/ChooseCategoryItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import TextLine from "../../Components/TextLine";
+
+function ChooseCategoryItem ({category}) {
+
+    return (
+        <Link className="link" to={category.url}>
+            <div data-testid="category-item" className="p-0 ProductPage-circle-content fd-col">
+                <div className="txt-blue bg-white row jc-center HomePage-border-circle ChooseCategoryItem-conteiner-img">
+                    <img src={category.icon} alt="productos" width="50"></img>
+                </div>
+                <TextLine className="txt-white HomePage-fs-14" text={category.name}/>
+            </div>
+        </Link>
+    )
+}
+
+
+
+export default ChooseCategoryItem
