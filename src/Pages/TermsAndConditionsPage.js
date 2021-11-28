f9f9c223386a44948ae6a09c4230759a3730770e
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-11-27 22:09:17 -0300

    added TextPageLayout in TermsAndConditionPage.js

--- src/Pages/TermsAndConditionsPage.js
+++ src/Pages/TermsAndConditionsPage.js
@@ -1,14 +1,15 @@
-import DistractionFreeLayout from "../Layouts/DistractionFreeLayout";
+
+import TextPageLayout from "../Layouts/TextPageLayout";
 import info from "./TermsAndConditionsPage/info";
 
 let titulo = "Términos y condiciones de uso del Sitio"
 let text = info
 function TermsAndConditionsPage() {
     return(
-        <DistractionFreeLayout>
-            <div className="container bg-light-grey ">
-                <div className="row txt-bold ProductPage-fs-24">
-                    <div className="col">
+        <TextPageLayout showInfoVotation={true}>
+            <div className="container padding-none">
+                <div className="row txt-bold ProductPage-fs-24 padding-none m-bottom-5  ">
+                    <div className="col padding-none m-bottom-5 m-top-5">
                         {titulo}
                     </div>
                 </div>
@@ -18,7 +19,7 @@ function TermsAndConditionsPage() {
                     </div>
                 </div>
             </div>
-        </DistractionFreeLayout>
+        </TextPageLayout>
     ) 
   }
   
