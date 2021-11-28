cc26ee801dd480d9d7cd14c6a116b021753a0d86
Jora_c <84633047+jocardozo@users.noreply.github.com>
2021-11-28 19:24:37 -0300

    Add changes in routes, in footer info & financial user page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ProductPage from './Pages/Public/ProductPage/ProductPage';
 import ChooseRootCategory from './Pages/PublishingProcess/ChooseRootCategory';
 import ConfirmClassification from './Pages/Public/ConfirmClassification';
 import ShoppingHistoryPage from './Pages/ShoppingHistoryPage/ShoppingHistoryPage'
+import InfoFinancialUserPage from './Pages/Public/InfoFinancialUserPage';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/sell/chooseCategory" element={<ChooseRootCategory />} />
         <Route path="/sell/confirm-classification" element={<ConfirmClassification />} />
         <Route path="/shopping-history" element={<ShoppingHistoryPage/>} />
+        <Route path="/financial-user-info" element={<InfoFinancialUserPage/>}/> 
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Public/InfoFinancialUserPage.js
+++ src/Pages/Public/InfoFinancialUserPage.js
@@ -1,36 +1,141 @@
-import Navbar from "../Blocks/Navbar"
-import TextInput from "../../Components/TextInput"
+import TextPageLayout from "../../Layouts/TextPageLayout"
 import TextLink from "../../Components/TextLink"
 
 function InfoFinancialUserPage (){
     const title = 'Información al usuario financiero'
     const initialText = 'Contás con nuestra ayuda para consultas o reclamos en relación a tu crédito de consumo y/o préstamo personal. Tenemos el compromiso de responderte en un plazo máximo de 10 días hábiles a partir de tu contacto.'
     const canaltext = 'Canales'
-    const tel = 'Teléfono: +54 11 51290410' // va dos veces! 
-    const mail = 'E-mail: usuarios_financieros@mercadopago.com' // va dos veces! 
-    const helpPort = 'Portal de Ayuda: Mercado Crédito.'
+    const tel = 'Teléfono: +54 11 51290410'
+    const mail = 'E-mail: usuarios_financieros@mercadopago.com'
+    const helpPort = 'Portal de Ayuda:'
+    const port = 'Mercado Crédito.'
     const areaRespons = 'Responsables de Area'
     const titular = 'Responsable Titular: Pedro Kudrnac'
     const suplente = 'Responsable Suplente: Mariano Aztiria'
     const direction = 'Dirección: Posta 4789, piso 6 - C1430EKG - Ciudad Autónoma de Buenos Aires'
-    const secondText = 'En caso de no estar de acuerdo con nuestra respuesta o si tardamos más de 10 días hábiles en hacerlo, podés comunicarte con la Gerencia Principal de Protección a Usuarios Financieros a través de su sitio: www.usuariosfinancieros.gob.ar.'
+    const secondText = 'En caso de no estar de acuerdo con nuestra respuesta o si tardamos más de 10 días hábiles en hacerlo, podés comunicarte con la Gerencia Principal de Protección a Usuarios Financieros a través de su sitio:'
+    const secondTextLink = 'www.usuariosfinancieros.gob.ar.'
     const rights = 'Tus derechos como usuario de servicios financiero:'
+    const imgBc = 'public/210122132030_05BCRA_Normas2019.jpg'
+    const imgDownProducts = 'public/210122131750_01BCRA_Normas2019.jpg'
     const rates = 'Conocé nuestras tarifas:'
     const ratesText = 'Cargos y comisiones Mercado Libre'
     const prodAndFees = 'Productos y tasas:'
     const prodAndFeesText = 'Líneas de consumo y préstamos personales en Mercado Libre'
 
     return (
-        <InfoFinancialUserPage>
-            <Navbar/>
-            <TextInput/>
-            <h1>{title}</h1>
-            <div>
-                <div>
-                    
+        <TextPageLayout showInfoVotation={true}>
+            <InfoFinancialUserPage>
+                <div className="row">
+                    <div className="col">
+                        <p>{title}</p>
+                    </div>
                 </div>
-            </div>
-            
-        </InfoFinancialUserPage>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p>{initialText}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{canaltext}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{tel}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{mail}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{helpPort}</p>
+                            <TextLink url={'/login'}>{port}</TextLink>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{areaRespons}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{titular}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{suplente}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{direction}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{mail}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{tel}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{secondText}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <TextLink>{secondTextLink}</TextLink>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{rights}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <img src={imgBc}></img>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <img src={imgDownProducts}></img>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{rates}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <TextLink url="/login">{ratesText}</TextLink>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{prodAndFees}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <TextLink url="/login">{prodAndFeesText}</TextLink>
+                        </div>
+                    </div>
+                </div>
+            </InfoFinancialUserPage>
+        </TextPageLayout>
         )
 }
+export default InfoFinancialUserPage
