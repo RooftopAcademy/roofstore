4790959755412d83ec6dfb3360d6d34813f144b9
Mauri548 <mauricioferreyra548@gmail.com>
2021-11-28 00:34:33 -0300

    added route for page "que queres publicar"

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './Pages/Public/LoginPage';
 import RegisterPage from './Pages/Public/RegisterPage';
 import OffersPage from './Pages/Public/OffersPage';
 import ProductPage from './Pages/Public/ProductPage/ProductPage';
+import ChooseRootCategory from './Pages/PublishingProcess/ChooseRootCategory';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/offers" element={<OffersPage />} />
         <Route path="/product" element={<ProductPage />} />
+        <Route path="/sell/chooseCategory" element={<ChooseRootCategory />} />
       </Routes>
     </BrowserRouter>
   );
