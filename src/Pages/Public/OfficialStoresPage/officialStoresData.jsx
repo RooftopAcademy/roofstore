192391c659099637891ed84e89186e67cf27e6e4
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-30 01:54:40 -0300

    Added data of official stores

---
+++ src/Pages/Public/OfficialStoresPage/officialStoresData.jsx
@@ -0,0 +1,48 @@
+export const officialStoresData = {
+    computation: {
+        title: 'Computación',
+        brandsData:
+            [
+                {
+                    brandName: 'Lenovo',
+                    src: 'http://localhost:3000/officialStoresImgs/official1.jpg',
+                    to: '/',
+                },
+                {
+                    brandName: 'SanDisk',
+                    src: 'http://localhost:3000/officialStoresImgs/official2.jpg',
+                    to: '/',
+                },
+                {
+                    brandName: 'Dell',
+                    src: 'http://localhost:3000/officialStoresImgs/official3.jpg',
+                    to: '/'
+                },
+                {
+                    brandName: 'Cisco',
+                    src: 'http://localhost:3000/officialStoresImgs/official4.jpg',
+                    to: '/',
+                },
+                {
+                    brandName: 'Gx Gaming',
+                    src: 'http://localhost:3000/officialStoresImgs/official5.jpg',
+                    to: '/',
+                },
+                {
+                    brandName: 'Thermaltake',
+                    src: 'http://localhost:3000/officialStoresImgs/official6.jpg',
+                    to: '/',
+                },
+                {
+                    brandName: 'Gadnic',
+                    src: 'http://localhost:3000/officialStoresImgs/official7.jpg',
+                    to: '/',
+                },
+                {
+                    brandName: 'HP',
+                    src: 'http://localhost:3000/officialStoresImgs/official8.jpg',
+                    to: '/',
+                }
+            ]
+    }
+}
