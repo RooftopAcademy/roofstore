825be70ad1b58e53dc45a8e7b5340fa77eb17f64
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-01 14:18:22 -0300

    Added map for manage more category block

--- src/Pages/Public/OfficialStoresPage.jsx
+++ src/Pages/Public/OfficialStoresPage.jsx
@@ -1,13 +1,25 @@
 import OfficialStoresBlock from "./OfficialStoresPage/OfficialStoresBlock"
 import { officialStoresData } from "./OfficialStoresPage/officialStoresData"
 import WebsiteLayout from "../../Layouts/WebsiteLayout"
+import { forgetLargeIcon } from '../../Components/SearchHelp/svgIcons'
+import InputStores from './OfficialStoresPage/InputStores'
 function OfficialStores() {
     const textButton = 'Ver todas las tiendas'
     return (
         <WebsiteLayout>
+            <div className="row padding-none">
+                <div className="col padding-none bg-main">
+                    <img className="Stores-image-banner w-100" src={"https://http2.mlstatic.com/resources/largesellers/header-web_mobile-768-HSP@2x.png"} alt={"Offical Stores Banner"} />
+                </div>
+            </div>
             <div className="row bg-main ">
-                <div className='col'>
-                    <OfficialStoresBlock category={officialStoresData.computation} />
+                <div className="col">
+                    <InputStores />
+                    {
+                        officialStoresData.map(category => {
+                            return (<OfficialStoresBlock category={category} />)
+                        })
+                    }
                     <div className="row jc-center">
                         <div className="col">
                             <button className="bg-blue txt-white p-form round txt-bold border-none fz-m ">{textButton}</button>
@@ -15,7 +27,8 @@ function OfficialStores() {
                     </div>
                 </div>
             </div>
-        </WebsiteLayout>
+        </WebsiteLayout >
     )
 }
+
 export default OfficialStores
