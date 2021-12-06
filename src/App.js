08ae32a4ccee6638ac704c26a02c000eb2320041
Kevin Dev <kevin.developer71@gmail.com>
2021-12-05 22:18:31 -0600

    adding authentication check in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import CartPage from './Pages/Public/CartPage';
-import CartUnlogged from './Pages/Public/CartUnlogged';
+import CartUnlogged from './Pages/Public/CartUnloggedPage';
 import CategoriesPage from './Pages/CategoriesPage/CategoriesPage';
 import ChooseItemTitlePage from './Pages/PublishingProcess/ChooseItemTitlePage';
 import ChooseRootCategory from './Pages/PublishingProcess/ChooseRootCategory';
@@ -20,18 +20,21 @@ import ProductListPage from './Pages/Public/ProductListPage'
 import RegisterPage from './Pages/Public/RegisterPage';
 import ShoppingHistoryPage from './Pages/ShoppingHistoryPage/ShoppingHistoryPage'
 import TermsAndConditionsPage from './Pages/TermsAndConditionsPage/TermsAndConditionsPage';
+import useAuth from './hooks/useAuth';
 
 function App() {
+
+  const { user } = useAuth()
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ayuda/terminos-y-condiciones" element={<TermsAndConditionsPage />} />
         <Route path="/ayuda/defensa-del-consumidor" element={<DefensaConsumidorPage />} />
-        <Route path="/cart" element={<CartPage/>} />
-        <Route path="/cart-unlogged" element={<CartUnlogged/>}/> 
-        <Route path="/categories" element={<CategoriesPage/>} />
-        <Route path="/financial-user-info" element={<InfoFinancialUserPage/>}/> 
+        <Route path="/cart" element={user ? <CartPage /> : <CartUnlogged />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/financial-user-info" element={<InfoFinancialUserPage />} />
         <Route path="/help" element={<HelpPage />} />
         <Route path="/help/:id" element={<HelpPageCategory />} />
         <Route path="/login" element={<LoginPage />} />
@@ -40,10 +43,10 @@ function App() {
         <Route path="/product-list" element={<ProductListPage />} />
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/sell/chooseCategory" element={<ChooseRootCategory />} />
+        <Route path="/sell/chooseCategory" element={user ? <ChooseRootCategory /> : <LoginPage />} />
         <Route path="/sell/confirm-classification" element={<ConfirmClassification />} />
-        <Route path="/shopping-history" element={<ShoppingHistoryPage />} />
-        <Route path="/sell/item-title" element={<ChooseItemTitlePage />} />
+        <Route path="/shopping-history" element={user ? <ShoppingHistoryPage /> : <LoginPage />} />
+        <Route path="/sell/item-title" element={user ? <ChooseItemTitlePage /> : <LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
