513fd3c88dee2f972b4362745dfbb6a71a06681f
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-12-09 22:42:53 -0300

    fix: Updated paths in FoterLogin.js

--- src/Blocks/Footer/FooterLogin.js
+++ src/Blocks/Footer/FooterLogin.js
@@ -6,7 +6,7 @@ function FooterLogin() {
             <div className='col'>
                 <TextLink url={'/login'} className={'txt-blue m-right-0'}>Ingresá</TextLink>
                 <span className='br-right'/>
-                <TextLink url={'/login'} className={'txt-blue m-left-0'}>Creá tu cuenta</TextLink>
+                <TextLink url={'/register'} className={'txt-blue m-left-0'}>Creá tu cuenta</TextLink>
             </div>
         </div>
     )
