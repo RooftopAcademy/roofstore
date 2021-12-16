08315a5b630fc99ec58faf86a4711a60418655fe
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-16 09:00:37 -0300

    ScrollTop wrapp all the childres

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import ProductPage from './Pages/Public/ProductPage/ProductPage';
 import ProductListPage from './Pages/Public/ProductListPage'
 import RegisterPage from './Pages/Public/RegisterPage';
-import ScrollTop from './hooks/scrollTop';
+import ScrollTop from './Components/ScrollTop';
 import ShoppingHistoryPage from './Pages/ShoppingHistoryPage/ShoppingHistoryPage'
 import TermsAndConditionsPage from './Pages/TermsAndConditionsPage/TermsAndConditionsPage';
 
@@ -27,27 +27,28 @@ function App() {
 
   return (
     <BrowserRouter>
-      <ScrollTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/ayuda/terminos-y-condiciones" element={<TermsAndConditionsPage />} />
-        <Route path="/ayuda/defensa-del-consumidor" element={<DefensaConsumidorPage />} />
-        <Route path="/cart" element={<PrivateRoute><CartPage /></PrivateRoute>} />
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/financial-user-info" element={<InfoFinancialUserPage />} />
-        <Route path="/help" element={<HelpPage />} />
-        <Route path="/help/:id" element={<HelpPageCategory />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/offers" element={<OffersPage />} />
-        <Route path="/officialStores" element={<OfficialStores />} />
-        <Route path="/product-list" element={<ProductListPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/sell/chooseCategory" element={<PrivateRoute><ChooseRootCategory /></PrivateRoute>} />
-        <Route path="/sell/confirm-classification" element={<PrivateRoute><ConfirmClassification /></PrivateRoute>} />
-        <Route path="/shopping-history" element={<PrivateRoute><ShoppingHistoryPage /></PrivateRoute>} />
-        <Route path="/sell/item-title" element={<PrivateRoute><ChooseItemTitlePage /></PrivateRoute>} />
-      </Routes>
+      <ScrollTop>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ayuda/terminos-y-condiciones" element={<TermsAndConditionsPage />} />
+          <Route path="/ayuda/defensa-del-consumidor" element={<DefensaConsumidorPage />} />
+          <Route path="/cart" element={<PrivateRoute><CartPage /></PrivateRoute>} />
+          <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/financial-user-info" element={<InfoFinancialUserPage />} />
+          <Route path="/help" element={<HelpPage />} />
+          <Route path="/help/:id" element={<HelpPageCategory />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/offers" element={<OffersPage />} />
+          <Route path="/officialStores" element={<OfficialStores />} />
+          <Route path="/product-list" element={<ProductListPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/sell/chooseCategory" element={<PrivateRoute><ChooseRootCategory /></PrivateRoute>} />
+          <Route path="/sell/confirm-classification" element={<PrivateRoute><ConfirmClassification /></PrivateRoute>} />
+          <Route path="/shopping-history" element={<PrivateRoute><ShoppingHistoryPage /></PrivateRoute>} />
+          <Route path="/sell/item-title" element={<PrivateRoute><ChooseItemTitlePage /></PrivateRoute>} />
+        </Routes>
+      </ScrollTop>
     </BrowserRouter>
   )
 }
