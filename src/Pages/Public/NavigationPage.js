05eb805ae5f2617abd8bf006c0624c0f0e2161cc
Fernando Alba <fernando.alba@live.com.ar>
2021-12-17 11:23:26 -0300

    feat: create navigation page

---
+++ src/Pages/Public/NavigationPage.js
@@ -0,0 +1,15 @@
+import WebsiteLayout from "../../Layouts/WebsiteLayout"
+
+function NavigationPage() {
+    return (
+        <WebsiteLayout>
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+        </WebsiteLayout>
+    )
+}
+
+export default NavigationPage
