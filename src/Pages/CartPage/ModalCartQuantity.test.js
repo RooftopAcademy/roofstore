1fc36267e6aff504cac44a65a3d3042162fd3489
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-12-17 12:09:03 -0300

    Add new test for ModalCartQuantity component

---
+++ src/Pages/CartPage/ModalCartQuantity.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import CartItem from './CartItem';
+import userEvent from '@testing-library/user-event'
+
+const item = {
+  "id" : 1,
+  "image": "https://http2.mlstatic.com/D_Q_NP_2X_649806-MLA47931899819_102021-G.webp",
+  "title": "Notebook Hp 14-cf2074la Intel Core I5 10210u 8gb 256gb Win10",
+  "price": 99.999,
+  "discount": "8% OFF",
+  "installments": "9x $ 11.111 sin interés",
+  "freeShipping": true,
+  "fullShipping": true,
+  "cant": 1
+}
+
+describe("ModalCartQuantity", () => {
+  beforeEach(() => {
+    render (
+      <BrowserRouter>
+        <CartItem item={item} />
+      </BrowserRouter>
+    )
+  })
+
+  test("render the modal when clicking on the select quantity button", () => {
+    const openModalBtn = screen.getByText(/1 u./i)
+    userEvent.click(openModalBtn)
+    const modalCartQuantity = screen.getByTestId('modal-cart-cuantity')
+    expect(modalCartQuantity).toBeInTheDocument()
+  })
+})
