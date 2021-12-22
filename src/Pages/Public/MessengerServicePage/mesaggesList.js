a56eb5f3b197d1d53440951c36439699a0e77f50
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-12-22 00:13:43 -0300

    feat: added new component for render messages

---
+++ src/Pages/Public/MessengerServicePage/mesaggesList.js
@@ -0,0 +1,74 @@
+const messagesList = 
+[
+    {
+    id: 1,
+    date: '14 de Noviembre',
+    messages: [
+        {   
+        id: 1,
+        iniciales : "NN",
+        message : "Buenas tardes, gracias por la compra",
+        time : "11:47",
+        send: false,
+        },
+        {   
+        id: 2,
+        iniciales : "NN",
+        message : "Buenas tardes",
+        time : "11:51",
+        send: true,
+        },
+        {   
+        id: 3,
+        iniciales : "NN",
+        message : "Cualquier duda que tengas podes escribirnos",
+        time : "11:53",
+        send: false,
+        },
+        {   
+        id: 4,
+        iniciales : "",
+        message : "Bueno, muchas gracias",
+        time : "11:57",
+        send: true,
+        },
+        {   
+        id: 5,
+        iniciales: "",
+        message: "Necesito una factura del producto",
+        time: "11:58",
+        send: true,
+        }
+    ]
+    },
+    {
+    id: 2,
+    date: '15 de Noviembre',
+    messages: [
+        {   
+        id: 1,
+        iniciales: "NN",
+        message: "Tu paquete ya fue entregado al correo",
+        time : "09:49",
+        send: false,
+        },
+        {   
+        id: 2,
+        iniciales: "NN",
+        message: "Te adjunto la factura que nos solicitaste",
+        docs: true,
+        time: "09:50",
+        send: false,
+        },
+        {   
+        id: 3,
+        iniciales: "",
+        message: "Gracias",
+        time: "09:52",
+        send: true,
+        }
+     ]
+    },
+]
+  
+  export default messagesList
