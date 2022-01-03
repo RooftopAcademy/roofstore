465d2f338699c3b36aba1b332ffc98e1487edbb2
alejandro <info@alezvi.com.ar>
2022-01-03 15:40:41 -0300

    pwa initialization

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { AuthProvider } from './context/AuthContext'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as fb from './firebase'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +14,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 )
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
