c9dc7fa7c483b339045a1c45d39f40da9789c909
Kevin Dev <kevin.developer71@gmail.com>
2022-01-04 10:40:23 -0600

    config de firebase para notifications

--- src/firebase.js
+++ src/firebase.js
@@ -2,35 +2,39 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const messaging = getMessaging()
 
-const vapid = process.env.REACT_FIREBASE_VAPID
+export const askForNotificationPermission = () => {
+  const messaging = getMessaging();
 
-getToken(messaging, { vapidKey: vapid }).then((currentToken) => {
-  if (currentToken) {
-      console.log('CURRENT TOKEN', currentToken)
-    // Send the token to your server and update the UI if necessary
-    // ...
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
-});
+  const vapid = process.env.REACT_FIREBASE_VAPID;
 
-export default {
-    app,
-    messaging
+  getToken(messaging, { vapidKey: vapid })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log('CURRENT TOKEN', currentToken);
+        // Send the token to your server and update the UI if necessary
+        // ...
+      } else {
+        // Show permission request UI
+        console.log(
+          'No registration token available. Request permission to generate one.'
+        );
+        // ...
+      }
+    })
+    .catch((err) => {
+      console.log('An error occurred while retrieving token for notification. ', err);
+      // ...
+    });
 }
+
+export default app;
