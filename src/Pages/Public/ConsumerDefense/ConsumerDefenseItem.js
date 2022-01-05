6ef7ecaa6f158374d9947778a41e93875b60346b
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-05 19:30:38 -0300

    Saving the text in variables

--- src/Pages/Public/ConsumerDefense/ConsumerDefenseItem.js
+++ src/Pages/Public/ConsumerDefense/ConsumerDefenseItem.js
@@ -1,14 +1,19 @@
 function ConsumerDefenseItem({ title, children, lastModifiedDate }) {
+  const goBackUpText = 'Vovler arriba'
+  const lastModificationText = 'Última modificación:'
+
   return (
     <section className="border-top-footer">
       <div className="row DefensaConsumidor-flex-end padding-none">
         <button className="link-color ProductPage-keyword-information-button DefensaConsumidor-bg-transparent">
-          Volver arriba
+          {goBackUpText}
         </button>
       </div>
 
       <h4 className="DefensaConsumidor-subtitle m-left-5">{title}</h4>
-      {lastModifiedDate && <p className="m-left-5">Última modificación: {lastModifiedDate}</p>}
+        {
+          lastModifiedDate && <p className="m-left-5">{lastModificationText} {lastModifiedDate}</p>
+        }
       <div>&nbsp;</div>
       <div className="DefensaConsumidor-section">
         {children}
