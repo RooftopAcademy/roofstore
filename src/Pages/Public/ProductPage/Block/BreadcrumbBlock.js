b2038696c6f5a9a98cf439983c9a819cf791f9b1
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-05 23:42:50 -0300

    modifying the component so that it renders according to the array passed to it

--- src/Pages/Public/ProductPage/Block/BreadcrumbBlock.js
+++ src/Pages/Public/ProductPage/Block/BreadcrumbBlock.js
@@ -1,17 +1,18 @@
-function BreadcrumbBlock() {
+function BreadcrumbBlock({ categories }) {
+  /**
+   * Si es la primera categoria no se le agrega un icono de flecha antes de la palabra.
+   */
+  const categoriesItems = categories.map((category, i) => {
+    if (i === 0) return <span>{category} </span>
+    return <span><i className="fas fa-angle-right" /> {category} </span>
+  })
+
   return (
     <section>
-      <div className="row">
-        <span>Deportes</span>
-        <span>
-          <i className="fas fa-angle-right" /> Bicicletas
-        </span>
-        <span>
-          <i className="fas fa-angle-right" /> Rodado 29
-        </span>
-        <span>
-          <i className="fas fa-angle-right" /> Mountain Bike
-        </span>
+      <div className="row OffersPAge-txt-m ">
+        <div className="col padding-none">
+          {categoriesItems}
+        </div>
       </div>
     </section>
   )
