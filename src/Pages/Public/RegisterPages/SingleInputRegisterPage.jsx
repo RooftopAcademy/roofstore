ef6c72af5acb78df3c68bab46de4ff67d46b8b4c
Fernando Alba <fernando.alba@live.com.ar>
2022-01-07 11:26:57 -0300

    fix: pass input id by props in SingleInputRegisterPage.jsx

--- src/Pages/Public/RegisterPages/SingleInputRegisterPage.jsx
+++ src/Pages/Public/RegisterPages/SingleInputRegisterPage.jsx
@@ -1,13 +1,13 @@
 import FormInput from "../../../Components/FormInput";
 
-function SingleInputRegisterPage({ optionActive, className, setOptionActive, title, explication, buttonText, holder = '' }) {
+function SingleInputRegisterPage({ optionActive, className, setOptionActive, title, explication, buttonText, holder = '', formInputId }) {
     return (
         <div className={`row ${className}`}>
             <div className="col w-100">
                 <h1 className="title m-top-1 m-bottom-2">{title}</h1>
                 <p className="fz-sm txt-grey-copyright m-bottom-5">{explication}</p>
                 <form className="d-flex fd-col">
-                    <FormInput id="mobile-input" type="text" className="input round p-form" holderBottom={holder} />
+                    <FormInput id={formInputId} type="text" className="input round p-form" holderBottom={holder} />
                     <button type="button" onClick={() => { setOptionActive(optionActive + 1) }} className=" m-top-5 bg-blue txt-white p-form round txt-bold border-none fz-m">{buttonText}</button>
                 </form>
             </div>
