a2eef9383865fb003a37cf736901372a53553f37
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-07 10:34:12 -0300

    Refactored PaymentBlock and moved to public/images folder the images used in the component

--- src/Pages/Public/ProductPage/Block/PaymentBlock.js
+++ src/Pages/Public/ProductPage/Block/PaymentBlock.js
@@ -1,72 +1,123 @@
 function PaymentBlock() {
+  const paymentMethodsText = 'Medios de pago'
+  const twelvePaymentsText = 'Hasta 12 cuotas sin tarjeta'
+  const creditCardsText = 'Tarjetas de crédito'
+  const interestFreeText = '¡Cuotas sin interés con bancos seleccionados!'
+  const debitCardsText = 'Tarjetas de débito'
+  const cashText = 'Efectivo'
+  const knowOtherMethods = 'Conocer otros medios de pago'
+
+  const paymentMethods = {
+    "card": {
+      "credit": [
+        {
+          "id": 1,
+          "name": "Visa",
+          "img": "/images/visa.png"
+        },
+        {
+          "id": 2,
+          "name": "American Express",
+          "img": "/images/amex.png",
+        },
+        {
+          "id": 3,
+          "name": "Master Card",
+          "img": "/images/mastercard.png",
+        },
+        {
+          "id": 4,
+          "name": "Naranja",
+          "img": "/images/naranja.png",
+        },
+      ],
+      "debit": [
+        {
+          "id": 1,
+          "name": "Visa Debito",
+          "img": "/images/visa-debito.png",
+        },
+        {
+          "id": 2,
+          "name": "Master Card",
+          "img": "/images/mastercard-debito.png",
+        },
+        {
+          "id": 3,
+          "name": "Cabal",
+          "img": "/images/cabal-debito.png",
+        },
+        {
+          "id": 4,
+          "name": "Maestro",
+          "img": "/images/maestro.png",
+        },
+      ],
+    },
+    "cash": [
+      {
+        "id": 1,
+        "name": "Rapipago",
+        "img": "/images/rapipago.png",
+      },
+      {
+        "id": 2,
+        "name": "Pago Fácil",
+        "img": "/images/pago-facil.png",
+      },
+    ]
+  }
+
   return (
     <section className="row shadow-sm ProductPage-w-100">
       <div className="col ProductPage-w-100">
-        <h3 className="ProductPage-section-title m-bottom-2">Medios de pago</h3>
-        <p>Hasta 12 cuotas sin tarjeta</p>
+        <h3 className="ProductPage-section-title m-bottom-2">{paymentMethodsText}</h3>
+        <p>{twelvePaymentsText}</p>
         <div className="row m-bottom-2">
           <img
-            src="https://http2.mlstatic.com/secure/payment-logos/v2/payment-logo-mla-consumer_credits-medium_v_91cc43d5a1.png"
+            src="/images/mercado-credito.png"
             alt="mercado credito"
           />
         </div>
-        <p>Tarjetas de crédito</p>
+        <p>{creditCardsText}</p>
         <p className="ProductPage-txt-light-grey">
-          ¡Cuotas sin interés con bancos seleccionados!
+          {interestFreeText}
         </p>
+
         <div className="row m-bottom-2">
-          <img
-            src="/images/visa.png"
-            alt="visa"
-          />
-          <img
-            src="/images/amex.png"
-            alt="American Express"
-          />
-          <img
-            src="/images/mastercard.png"
-            alt="master card"
-          />
-          <img
-            src="/images/naranja.png"
-            alt="naranja"
-          />
+          {paymentMethods.card.credit.map(({id, name, img}) => (
+            <img
+              key={id}
+              src={img}
+              alt={name}
+            />
+          ))}
         </div>
-        <p>Tarjetas de débito</p>
+        <p>{debitCardsText}</p>
         <div className="row m-bottom-2">
-          <img
-            src="/images/visa-debito.png"
-            alt="visa debito"
-          />
-          <img
-            src="/images/mastercard-debito.png"
-            alt="master card"
-          />
-          <img
-            src="/images/cabal-debito.png"
-            alt="cabal"
-          />
-          <img
-            src="/images/maestro.png"
-            alt="maestro"
-          />
+          {paymentMethods.card.debit.map(({id, name, img}) => (
+            <img
+              key={id}
+              src={img}
+              alt={name}
+            />
+          ))}
         </div>
-        <p>Efectivo</p>
+        <p>{cashText}</p>
         <div className="row">
           <div className="col padding-none">
-            <img
-              src="https://http2.mlstatic.com/secure/payment-logos/v2/payment-logo-mla-rapipago-medium_v_91cc43d5a1.png"
-              alt="rapipago"
-            />
-            <img
-              className="m-left-5"
-              src="https://http2.mlstatic.com/secure/payment-logos/v2/payment-logo-mla-pagofacil-medium_v_91cc43d5a1.png"
-              alt="pago facil"
-            />
+            {paymentMethods.cash.map(({id, name, img}) => (
+              <img
+              className="m-right-5"
+                key={id}
+                src={img}
+                alt={name}
+              />
+            ))}
           </div>
         </div>
         <button className="rounded txt-blue bg-white p-0 input ProductPage-btn-left">
-          Conocer otros medios de pago <i className="fas fa-angle-right" />
+          {knowOtherMethods} <i className="fas fa-angle-right" />
         </button>
       </div>
     </section>
