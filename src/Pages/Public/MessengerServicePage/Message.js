2fe69e505a1b67e5741476e707f6c760ba9dcc3f
Mauri548 <mauricioferreyra548@gmail.com>
2022-01-11 12:16:30 -0300

    change position audio

--- src/Pages/Public/MessengerServicePage/Message.js
+++ src/Pages/Public/MessengerServicePage/Message.js
@@ -4,7 +4,7 @@ import TextLine from "../../../Components/TextLine";
 function Message({item}) {
     let content
 
-    content = !item.audio ? (
+    content = (
         <div className={`container padding-none ${item.send ? 'OffersPage-p-0-left' : 'OffersPage-p-0-right'}`}>
             <div className={`row ${item.send? 'ProductPage-jc-right' : 'jc-start'}`}>
                 {!item.send ? 
@@ -16,14 +16,18 @@ function Message({item}) {
                 }
                 <div className="col padding-none">
                     <div className={`row grey-border rounded ${item.send ? 'bg-light-blue' : 'ProductPage-bg-light-grey m-left-5 fd-col'}`}>
-                        <TextLine text={item.message} className={"Footer-txt-start"}/>
-                        {item.docs  &&
-                            <div className="col w-100 padding-none m-top-0">
-                                <button className="bg-white ProductPage-border-none ProductPage-circle">
-                                    <Icon icon="file" className={"ProductPage-fs-36"}/>
-                                </button>
-                            </div>
-                        } 
+                        {!item.audio ? (
+                            <>
+                                <TextLine text={item.message} className={"Footer-txt-start"}/>
+                                {item.docs  &&
+                                    <div className="col w-100 padding-none m-top-0">
+                                        <button className="bg-white ProductPage-border-none ProductPage-circle">
+                                            <Icon icon="file" className={"ProductPage-fs-36"}/>
+                                        </button>
+                                    </div>
+                                }
+                            </>) : <audio id="player" className="m-left-5" controls src={item.message}></audio>
+                        }
                     </div>
                     <div className={`row padding-none ${item.send ? 'ProductPage-jc-right' : ''}`}>
                         <TextLine text={item.time} className={`ProductPage-txt-light-grey ProductPage-fs-14 ${!item.send ? 'Footer-txt-start m-left-5' : ''}`}/> 
@@ -31,8 +35,8 @@ function Message({item}) {
                 </div>
             </div>
         </div>
-    ) : <audio id="player" controls src={item.message}></audio>
-    
+    )
+
 	return content
 }
 
