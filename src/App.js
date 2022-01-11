c20fe10a014c530144bc913d8fa76691a86f3640
Fernando Alba <fernando.alba@live.com.ar>
2022-01-11 16:38:46 -0300

    fix: remove static routing in App.js

--- src/App.js
+++ src/App.js
@@ -3,31 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {useEffect} from 'react';
 import { onForegroundMessage } from './firebase';
 
-import AdminNavigationPage from './Pages/Public/AdminNavigationPage';
-import CategoriesPage from './Pages/CategoriesPage/CategoriesPage';
-import ChooseItemTitlePage from './Pages/PublishingProcess/ChooseItemTitlePage';
-import ChooseRootCategory from './Pages/PublishingProcess/ChooseRootCategory';
-import ConfirmClassification from './Pages/Public/ConfirmClassification';
-import ConsumerDefensePage from './Pages/Public/ConsumerDefense/ConsumerDefensePage';
-import FavouritesPage from './Pages/Public/FavouritesPage/FavouritesPage';
 import Geolocation from './Geolocation';
-import HelpPage from './Pages/Public/HelpPage';
-import HelpPageCategory from './Pages/Public/HelpPage/HelpPageCategory';
-import InfoFinancialUserPage from './Pages/Public/InfoFinancialUserPage';
-import LoginPage from './Pages/Public/LoginPage';
-import MarketPointPage from './Pages/MarketPointsPage/MarketPointsPage';
-import MessengerServicePage from './Pages/Public/MessengerServicePage/MessengerServicePage';
-import NavigationPage from './Pages/Public/NavigationPage';
-import OffersPage from './Pages/Public/OffersPage';
-import OfficialStores from './Pages/Public/OfficialStoresPage';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import ProductPage from './Pages/Public/ProductPage/ProductPage';
-import ProductList from './Pages/Public/ProductListPage'
-import RegisterPage from './Pages/Public/RegisterPage';
 import ScrollTop from './Components/ScrollTop';
-import ShoppingHistoryPage from './Pages/ShoppingHistoryPage/ShoppingHistoryPage'
-import TermsAndConditionsPage from './Pages/TermsAndConditionsPage/TermsAndConditionsPage';
-import UploadPhotoPage from './Pages/PublishingProcess/UploadPhotoPage';
 
 import routes from './routes'
 
@@ -42,38 +19,12 @@ function App() {
     <BrowserRouter>
       <ScrollTop>
         <Routes>
-            {routes.map(({path, element}, i) => {
-                return <Route key={i} path={path} element={element} />
-            })}
-
-          {/*<Route path="/" element={<HomePage />} />*/}
-          <Route path="/help/terms-and-conditions" element={<TermsAndConditionsPage />} />
-          <Route path="/help/defensa-del-consumidor" element={<ConsumerDefensePage />} />
-          {/*<Route path="/cart" element={<PrivateRoute><CartPage /></PrivateRoute>} />*/}
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/favourites" element={<PrivateRoute><FavouritesPage /></PrivateRoute>} />
-          <Route path="/financial-user-info" element={<InfoFinancialUserPage />} />
-          <Route path="/help" element={<HelpPage />} />
-          <Route path="/help/:id" element={<HelpPageCategory />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/market-points" element={<PrivateRoute><MarketPointPage /></PrivateRoute>} />
-          <Route path="/navigation" element={<NavigationPage />} />
-          <Route path="/navigation/admin" element={<AdminNavigationPage />} />
-          <Route path="/offers" element={<OffersPage />} />
-          <Route path="/product-list" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/sell/chooseCategory" element={<PrivateRoute><ChooseRootCategory /></PrivateRoute>} />
-          <Route path="/sell/confirm-classification" element={<PrivateRoute><ConfirmClassification /></PrivateRoute>} />
-          <Route path="/shopping-history" element={<PrivateRoute><ShoppingHistoryPage /></PrivateRoute>} />
-          <Route path="/shopping/messenger" element={<MessengerServicePage />} />
-          <Route path="/sell/item-title" element={<PrivateRoute><ChooseItemTitlePage /></PrivateRoute>} />
-          <Route path="/sell/upload-photo" element={<PrivateRoute><UploadPhotoPage /></PrivateRoute>} />
-          <Route path="/shopping-history" element={<PrivateRoute><ShoppingHistoryPage /></PrivateRoute>} />
-          <Route path="/stores" element={<OfficialStores />} />
+            {
+              routes.map(({path, element}, i) => <Route key={i} path={path} element={element} />)
+            }
         </Routes>
       </ScrollTop>
-    </BrowserRouter >
+    </BrowserRouter>
   )
 }
 
