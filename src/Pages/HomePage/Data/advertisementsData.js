23053ad3e20e1485d1c88e7d25b66272914f3bbb
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-10 23:23:35 -0300

    change url for local image

--- src/Pages/HomePage/Data/advertisementsData.js
+++ src/Pages/HomePage/Data/advertisementsData.js
@@ -4,28 +4,28 @@ const advertisementList = [
         'title': "PEQUEÑOS ELECTRO",
         'body1': "EQUIPÁ TU HOGAR",
         'body2': "EN 6X SIN INTERÉS",
-        'image': "https://http2.mlstatic.com/D_NQ_684569-MLA48136225881_112021-C.webp",
+        'image': "/images/cafetera.webp",
         'redirect': "/offers"
     },
     {
         'title': "PRODUCTOS PYMES",
         'body1': "OFERTAS HASTA",
         'body2': "12X SIN INTERÉS",
-        'image': "https://http2.mlstatic.com/D_NQ_757350-MLA48136577557_112021-C.webp",
+        'image': "/images/cafetera.webp",
         'redirect': "/offers"
     },
     {
         'title': "HERRAMIENTAS",
         'body1': "HASTA 40% OFF",
         'body2': "Y 6X SIN INTERÉS",
-        'image': "https://http2.mlstatic.com/D_NQ_971474-MLA48135672229_112021-C.webp",
+        'image': "/images/cafetera.webp",
         'redirect': "/offers"
     },
     {
         'title': "POINT MINI",
         'body1': "NUEVO POINT MINI",
         'body2': "¡COMPRALO AHORA!",
-        'image': "https://http2.mlstatic.com/D_NQ_902536-MLA47888168890_102021-C.webp",
+        'image': "/images/cafetera.webp",
         'redirect': "/login"
     }
 ]
