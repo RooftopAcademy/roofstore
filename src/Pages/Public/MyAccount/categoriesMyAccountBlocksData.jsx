880375c3f6d7d2335f8fc3eaab6dcd1861b34a79
Jora_c <84633047+jocardozo@users.noreply.github.com>
2022-01-11 11:49:04 -0300

    Carpeta de archivos MyAccountPage

---
+++ src/Pages/Public/MyAccount/categoriesMyAccountBlocksData.jsx
@@ -0,0 +1,135 @@
+const categoriesMyAccountData = [
+        {
+            title: 'Compras',
+            id: '1',
+            items: 
+                [
+                    {
+                        title: 'Compras',
+                        to: '/'
+                    },
+                    {
+                        title: 'Preguntas',
+                        to: '/'
+                    },
+                    {
+                        title: 'Favoritos',
+                        to: '/'
+                    }
+                ]
+        },
+        {
+            title: 'Ventas',
+            id: '2',
+            items: 
+                [
+                
+                    {
+                        title: 'Resumen',
+                        to: '/'
+                    },
+                    {
+                        title: 'Novedades',
+                        to: '/'
+                    },
+                    {
+                        title: 'Publicaciones',
+                        to: '/'
+                    },
+                    {
+                        title: 'Publicidad',
+                        to: '/'
+                    },
+                    {
+                        title: 'Preguntas',
+                        to: '/'
+                    },
+                    {
+                        title: 'Ventas',
+                        to: '/'
+                    },
+                    {
+                        title: 'Métricas',
+                        to: '/'
+                    },
+                    {
+                        title: 'Reputación',
+                        to: '/'
+                    },
+                    {
+                        title: 'Preferencias de ventas',
+                        to: '/'
+                    },
+                    {
+                        title: 'Centro de vendedores',
+                        to: '/'
+                    },            
+                ],
+        },
+        {
+            title: '',
+            id: '3',
+            items:
+                [
+                    {
+                        title: 'Mercado Shops',
+                        to: '/'
+                    }
+                ],
+        },
+        {
+            title: '',
+            id: '4',
+            items:
+                [
+                    {
+                        title: 'Peliculas y series',
+                        to: '/'
+                    }
+                ],
+        },
+        {
+            title: '',
+            id: '5',
+            items:
+                [
+                    {
+                        title: 'Facturación',
+                        to: '/'
+                    }
+                ],
+        },
+        {
+            title: 'Configuración',
+            id: '6',
+            items:
+                [
+                    {
+                        title: 'Mis datos',
+                        to: '/'
+                    },
+                    {
+                        title: 'Seguridad',
+                        to: '/'
+                    },
+                    {
+                        title: 'Privacidad',
+                        to: '/'
+                    },
+                    {
+                        title: 'Comunicaciones',
+                        to: '/'
+                    },
+                    {
+                        title: 'Alerta de búsqueda',
+                        to: '/'
+                    },
+                    {
+                        title: 'eShop',
+                        to: '/'
+                    }
+                ],
+        },
+
+    ]
+export default categoriesMyAccountData;
