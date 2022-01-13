5e269d4ddd578157bfb4b621e26ce58e738bc58a
Mauri548 <mauricioferreyra548@gmail.com>
2022-01-13 00:15:33 -0300

    created requests

---
+++ src/requests/categories.js
@@ -0,0 +1,19 @@
+export let getCategories = {
+  method: "get", 
+  url: "Categories/CategoriesPageData.json"
+}
+
+export let getPopularCategories = {
+  method: "get",
+  url: "Categories/categoriesData.json"
+}
+
+export let getOffersCategories = {
+  method: "get",
+  url: "Categories/categoryOffersData.json"
+}
+
+export let getCategoriesRoot = {
+  method: "get",
+  url: "Categories/categoriesRoot.json"
+}
---
+++ src/requests/mercadoPunto.js
@@ -0,0 +1,15 @@
+export let getBenefits = {
+  method: "get",
+  url: "MercadoPunto/mercadoPuntosBenefitsData.json"
+}
+
+export let getMoviesAndSeries = {
+  method: "get",
+  url: "MercadoPunto/moviesAndSerieData.json"
+}
+
+export let getNextLevelBenefit = {
+  method: "get",
+  url: "MercadoPunto/nextLevelBenefiData.json"
+}
+
