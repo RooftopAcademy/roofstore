f831cab7582d0ff5b4235b3c59affe7dd414311e
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-14 11:36:23 -0300

    modify condition of user

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -6,8 +6,9 @@ import useAuth from '../../hooks/useAuth'
 function PrivateRoute({ children }) {
   const { user } = useAuth()
   const { pathname } = useLocation()
+  console.log(user)
   
-  return user
+  return user.email !== ''
     ? children  // Si existe usuario se redenriza el children
     : pathname === '/cart' // Si no existe usuario y la ruta es /cart, se renderiza <CartUnlogged />
     ? <CartUnlogged /> // otras rutas se redirecciona a /login
