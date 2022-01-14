29013318ff45de032e20f86b5287c89cb9008c99
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-14 11:45:55 -0300

    delete console.log

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -6,8 +6,7 @@ import useAuth from '../../hooks/useAuth'
 function PrivateRoute({ children }) {
   const { user } = useAuth()
   const { pathname } = useLocation()
-  console.log(user)
-  
+
   return user.email !== ''
     ? children  // Si existe usuario se redenriza el children
     : pathname === '/cart' // Si no existe usuario y la ruta es /cart, se renderiza <CartUnlogged />
