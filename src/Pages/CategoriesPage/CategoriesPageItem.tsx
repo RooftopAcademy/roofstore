a77dbb00f4c3e6a6c0991b2527788db5008b1622
Scarlet <smichelle05a@gmail.com>
2022-01-27 19:11:46 -0300

    change CategoriesPageItem to .tsx and add types

--- src/Pages/CategoriesPage/CategoriesPageItem.tsx
+++ src/Pages/CategoriesPage/CategoriesPageItem.tsx
@@ -1,7 +1,9 @@
 import Icon from '../../Components/Icon'
 import TextLink from '../../Components/TextLink'
+import { Category } from '../../types/categories'
 
-function CategoriesPageItem ({categoryData}) {
+function CategoriesPageItem (props : {categoryData : Category}): JSX.Element {
+  const {categoryData} = props
   return(
       <TextLink className="txt-black d-flex w-100 p-0 br-btm justify-content-sb ai-center" url="/">
         {categoryData.category}
