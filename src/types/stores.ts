64abd0bb490443f8279d0443b8039e626c96ee71
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-01-27 15:22:39 -0300

    add types for official and local stores

---
+++ src/types/stores.ts
@@ -0,0 +1,13 @@
+export type OfficialStore = {
+  id: string;
+  title: string;
+  logo: string
+  backgroundImage: string,
+  to: string;
+}
+
+export type LocalStore = {
+  name: string;
+  logo: string;
+  offer: string;
+}
