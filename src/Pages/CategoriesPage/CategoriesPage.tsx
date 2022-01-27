e84bb8f5978f0ddc1555c53b9b427e93e8b4b312
Scarlet <smichelle05a@gmail.com>
2022-01-27 19:11:56 -0300

    change CategoriesPage to .tsx and add types

--- src/Pages/CategoriesPage/CategoriesPage.tsx
+++ src/Pages/CategoriesPage/CategoriesPage.tsx
@@ -2,28 +2,31 @@ import WebsiteLayout from '../../Layouts/WebsiteLayout';
 import CategoriesPageItem from './CategoriesPageItem';
 import useFetch from '../../hooks/useFetch';
 import { getCategories } from '../../requests/categories';
+import { Category } from '../../types/categories'
 
-function CategoriesPage () {
-  const {data: categoriesData} = useFetch(getCategories)
-  const services = categoriesData?.filter(category => category.section === 1)
-  const categories = categoriesData?.filter(category => category.section === 2)
-  const categorieText = 'Categorías'
+
+function CategoriesPage (): JSX.Element {
+
+  const {data: categoriesData} = useFetch<Category[]>(getCategories)
+  const services = categoriesData?.filter((category : Category) => category.section === 1)
+  const categories = categoriesData?.filter((category : Category) => category.section === 2)
+  const categoryText:string = 'Categorías'
 
   return (
     <WebsiteLayout>
       <div className="row bg-main">
         <div className="col w-100">
-          <h3 className='m-left-4'>{categorieText}</h3>
+          <h3 className='m-left-4'>{categoryText}</h3>
           <div className="row w-100">
             <div className="col bg-white rounded shadow-sm padding-none d-flex w-100 fd-col">
-              {services?.map((service) => {
+              {services?.map((service : Category) => {
                 return <CategoriesPageItem categoryData={service} key={service.id} />
               })}
             </div>
           </div>
           <div className="row w-100">
             <div className="col bg-white rounded shadow-sm padding-none d-flex w-100 fd-col">
-              {categories?.map((category) => {
+              {categories?.map((category : Category) => {
                 return <CategoriesPageItem categoryData={category} key={category.id} />
               })}
             </div>
